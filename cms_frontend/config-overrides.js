4a527cd7045c2c4a502697565c5a5fdfb19d0449
Jacky Xie <gokurohono@gmail.com>
2021-08-27 12:30:29 +1000

    templates

--- cms_frontend/config-overrides.js
+++ cms_frontend/config-overrides.js
@@ -3,7 +3,9 @@ const {alias} = require('react-app-rewire-alias')
 module.exports = function override(config) {
   alias({
     '@components': 'src/components',
-    '@assets' : 'src/assets'
+    '@assets' : 'src/assets',
+    '@pages' : 'src/pages',
+    '@css' : 'src/css'
   })(config)
 
   return config
---
+++ cms_frontend/src/pages/Dashboard.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Dashboard = () => {
+    return (
+        <div>
+            hello
+        </div>
+    )
+}
+
+export default Dashboard
