d88c1b2010b97ff3283d6b4e9286c6cdb69a3244
Jacky Xie <gokurohono@gmail.com>
2021-08-27 15:05:15 +1000

    added more folder structure

--- cms_frontend/src/pages/Dashboard.tsx
+++ cms_frontend/src/pages/Dashboard.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import SideBar from 'src/components/SideBar';
+import SideBar from 'src/components/SideBar/SideBar';
 import FileRenderer from 'src/components/FileRenderer/FileRenderer';
 
 
