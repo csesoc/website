a2b7a525e94427d56d10852c082093107f66cc0c
Jacky Xie <gokurohono@gmail.com>
2021-08-27 12:24:50 +1000

    adding alias library

---
+++ cms_frontend/config-overrides.js
@@ -0,0 +1,10 @@
+const {alias} = require('react-app-rewire-alias')
+
+module.exports = function override(config) {
+  alias({
+    '@components': 'src/components',
+    '@assets' : 'src/assets'
+  })(config)
+
+  return config
+}
