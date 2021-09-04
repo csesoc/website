1f242d72b75b09d234f24cde697863d5234f83fc
Jacky Xie <gokurohono@gmail.com>
2021-09-04 17:58:04 +1000

    rename

---
+++ Backend/internal/auth/query-test.go
@@ -0,0 +1,48 @@
+package auth
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	_ "github.com/lib/pq"
+)
+
+type Person struct {
+	Id    int    `json:"id"`
+	Name  string `json:"name"`
+	First string `json: "first"`
+	Pass  string `json: "pass"`
+}
+
+type People []Person
+
+// this function is just to get familiar with the code and have some example postgres handling code
+func GetAllUsers(w http.ResponseWriter, r *http.Request) {
+	// host = db -> because it depends on db in docker-compose file
+	connStr := "host=db port=5432 user=postgres password=postgres dbname=test_db sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// ping database
+	rows, _ := db.Query("SELECT * from person;")
+	log.Println("HALJSDLAJ")
+
+	var peopleList People
+	// put sql query into json
+	for rows.Next() {
+		item := Person{}
+		// log.Print(rows)
+		rows.Scan(&item.Id, &item.Name, &item.First, &item.Pass)
+		peopleList = append(peopleList, item)
+	}
+
+	json, _ := (json.Marshal(peopleList))
+	//log.Println(string(out))
+	fmt.Fprintf(w, string(json))
+
+}
--- Backend/main.go
+++ Backend/main.go
@@ -1,46 +1,16 @@
 package main
 
 import (
+	auth "DiffSync/internal/auth"
 	service "DiffSync/internal/service"
-	"database/sql"
 	"log"
 	"net/http"
-
-	_ "github.com/lib/pq"
 )
-
-type Person struct {
-	ID    int
-	name  string
-	first string
-	pass  string
-}
 
 func main() {
 	http.HandleFunc("/edit", service.EditEndpoint)
 	http.HandleFunc("/preview", service.PreviewHTTPHandler)
-	http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
-		// host = db -> because it depends on db in docker-compose file
-		connStr := "host=db port=5432 user=postgres password=postgres dbname=test_db sslmode=disable"
-		db, err := sql.Open("postgres", connStr)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		// ping database
-		rows, _ := db.Query("SELECT * from person;")
-		log.Println("HALJSDLAJ")
-		list := []Person{}
-
-		for rows.Next() {
-			item := Person{}
-			rows.Scan(&item.ID, &item.name, &item.first, &item.pass)
-			list = append(list, item)
-		}
-
-		log.Println(list)
-
-	})
+	http.HandleFunc("/getallusers", auth.GetAllUsers)
 	http.Handle("/", http.FileServer(http.Dir("./html")))
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
