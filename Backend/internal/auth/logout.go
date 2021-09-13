0caba1d8dd9a3d31a90acca89dff90d7818f3880
Jacky Xie <gokurohono@gmail.com>
2021-09-13 20:02:23 +1000

    logout functionality created and manually tested

--- Backend/internal/auth/logout.go
+++ Backend/internal/auth/logout.go
@@ -17,28 +17,35 @@ import (
 
 // expecting a post request with body of form data
 // expecting header to contain session-token
+// will perform the redirection in frontend
+// backend's job for logout is only to remove the HTTPONLY cookie
+
 func LogoutHandler(w http.ResponseWriter, r *http.Request) {
 
 	switch r.Method {
-	case "GET":
-		log.Print("logging out")
+	case "POST":
 		authenticated, _ := _session.IsAuthenticated(w, r)
+		log.Print(authenticated)
 		if authenticated {
+			// CORS headers
+			w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3001")
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
 			_session.RemoveSession(w, r)
-			// redirect to login page
-			http.RedirectHandler("http://localhost:3000/login", http.StatusMovedPermanently)
 			break
 		} else {
-			// throws an error to client
-			// 401 unauthorized
-			// redirects to page
-			_httpUtil.ThrowUnAuthorisedError(w)
+			// if session-token is not valid, it will still remove the current cookie the frontend
+			// is storing by returning a
+			// Set-Cookie: session-token="" header
+			w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3001")
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
+			_session.RemoveSession(w, r)
+			_httpUtil.ThrowRequestError(w, http.StatusUnauthorized, "unauthorized")
 			break
 		}
 
 	case "DEFAULT":
 		// only GET requests are allowed
-		_httpUtil.ThrowRequestError(w, 405, "Method Not allowed")
+		_httpUtil.ThrowRequestError(w, http.StatusMethodNotAllowed, "Method Not allowed")
 		break
 	}
 
