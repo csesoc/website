0df13f83eee30f941dbaea72dc00507ed9c70f0f
Nathan <z5257101@ad.unsw.edu.au>
2021-10-16 17:10:26 +1100

    Changed css to styled components

--- Cms_frontend/src/pages/Login.tsx
+++ Cms_frontend/src/pages/Login.tsx
@@ -1,7 +1,100 @@
 import React, { useState } from 'react';
-// import LoginForm from 'src/components/LoginScreen/LoginForm';
+//import LoginForm from 'src/components/LoginScreen/LoginForm';
 import {Typography, Button} from '@material-ui/core';
-import 'src/css/index.css'
+import styled from 'styled-components';
+
+const Container = styled.div`
+	margin: 0;
+  	padding: 0;
+ 	box-sizing: border-box;
+ 	font-family: montserrat, sans-serif;
+	height: 100vh;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	background-color: #1D1F20
+`
+
+const Input = styled.input`
+	appearance: none;
+	background: none;
+	border: none;
+	/* outline:none; */
+`
+const InputSubmit = styled.input.attrs({
+	type: 'submit',
+})`
+	display: inline-block;
+  padding: 10px 15px;
+  border-radius: 8px;
+  background-color: #F8F8F8;
+  background-size: 200%;
+  background-position: 0%;
+  transition: 0.4s;
+  cursor: pointer;
+`
+
+const Form = styled.form`
+	display: block;
+	position: relative;
+`
+const FormInner = styled.form`
+	position: relative;
+  display: block;
+  padding: 30px;
+  z-index: 2;
+`
+const FormGroup = styled.form`
+	display: block;
+  width: 300px;
+  margin-bottom: 15px;
+`
+const FormLabel = styled.label`
+	display: block;
+  color: #C4C4C4;
+  font-size: 12px;
+  margin-bottom: 5px;
+  transition: 0.4s;
+
+	&:focus-within {
+		color: #FFFFFF;
+	}
+`
+const FormError = styled.div`
+	display: block;
+  text-align: center;
+  color: #b62e2e;
+  padding: 0px 0px 15px;
+`
+const FormGroupInput = styled(Input)`
+	display: block;
+  width: 100%;
+  padding: 10px 15px;
+  background-color: #C4C4C4;
+  border-radius: 8px;
+  transition: 0.4s;
+
+	&:focus {
+		box-shadow: 0px 0px 3px rgba(0, 0, 0, 0.2);
+	}
+`
+const Img = styled.img`
+	display: block;
+  width: auto;
+  height: auto;
+  max-width: 200px;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 100px;
+`
+
+const H2 = styled.h2`
+	text-align: center;
+  color: #F8F8F8;
+  font-size: 28px;
+  font-weight: 500;
+  margin-bottom: 100px;
+`
 
 const LoginScreen: React.FC = () => {
 	const adminUser = {
@@ -27,7 +120,7 @@ const LoginScreen: React.FC = () => {
             })
 		} else {
 			console.log("Incorrect login details")
-			setError("Incorrect login detals")
+			setError("Incorrect login details")
 		}
 
 	}
@@ -38,36 +131,37 @@ const LoginScreen: React.FC = () => {
 
 	return (
         // <LoginForm Login={Login} error={error}/>
-        <form onSubmit={Login}>
-            <div className="form-inner">
-				
-				<h2>Team Login</h2>
-					<div className="form-group">
-						<label htmlFor="email">Email: </label>
-						<input type="email" name="email" id="email" onChange={
-							e => setDetails({...details, email: e.target.value})}  value = {details.email}/>
-					</div>
-					<div className="form-group">
-						<label htmlFor="password">Password:</label>
-						<input type="password" name="password" id="password" onChange={
-							e => setDetails({...details, password: e.target.value})}  value = {details.password}/>
-					</div>
-					{(error !== "" ? (<div className="error">{error}</div>) : "")}
-					{/* <input type="submit" value="LOGIN" /> */}
-					<Typography align='center'>
-						<Button 
-							style={{minWidth: '60px'}}
-							size="small"
-							onClick={Login} 
-							variant="contained" 
-							color="default">
-						Login
-						</Button>
-					</Typography>
-					
-            </div>
-			<img src="/images/csesocwhite-logo.png" alt="csesoc-logo" />
-        </form>
+		<Container>
+			<Form onSubmit={Login}>
+				<FormInner>
+					<H2>Team Login</H2>
+						<FormGroup>
+							<FormLabel htmlFor="email">Email: </FormLabel>
+							<FormGroupInput type="email" name="email" id="email" onChange={
+								e => setDetails({...details, email: e.target.value})}  value = {details.email}/>
+						</FormGroup>
+						<FormGroup>
+							<FormLabel htmlFor="password">Password:</FormLabel>
+							<FormGroupInput type="password" name="password" id="password" onChange={
+								e => setDetails({...details, password: e.target.value})}  value = {details.password}/>
+						</FormGroup>
+						{(error !== "" ? (<FormError>{error}</FormError>) : <FormError>ㅤ</FormError>)}
+						{/* <input type="submit" value="LOGIN" /> */}
+						<Typography align='center'>
+							<Button 
+								style={{minWidth: '60px'}}
+								size="small"
+								onClick={Login} 
+								variant="contained" 
+								color="default">
+							Login
+							</Button>
+						</Typography>
+						
+				</FormInner>
+				<Img src="/images/csesocwhite-logo.png" alt="csesoc-logo" />
+			</Form>
+		</Container>
 	);
 }
 
