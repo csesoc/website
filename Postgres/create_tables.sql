adf0294f6ffc300d7fa4302f7b82a333f5177ce8
Gary Sun <angary@canva.com>
2022-03-04 01:54:13 +1100

    Add sha256 encoding

--- Postgres/create_tables.sql
+++ Postgres/create_tables.sql
@@ -38,7 +38,7 @@ AS $$
 DECLARE
 BEGIN
   INSERT INTO person (Email, First_name, Password, isOfGroup)
-  VALUES (email, name, password, 2);
+  VALUES (email, name, sha256(password), 2);
 END $$;
 
 /* inserting two accounts into db */
