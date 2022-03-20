67749f6663ca4bc0e52297fe95f70bf2543b2726
Z5304138 <k.bowers@student.unsw.edu.au>
2022-03-20 20:27:55 +1100

    Implemented the path directory working with reactjs breadcrumbs

--- Frontend/src/packages/dashboard/Dashboard.tsx
+++ Frontend/src/packages/dashboard/Dashboard.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
+import { call } from 'redux-saga/effects';
 import styled from 'styled-components';
 import { Breadcrumbs, Link } from "@mui/material";
 
@@ -8,7 +9,8 @@ import SideBar from 'src/packages/dashboard/components/SideBar/SideBar';
 import Renderer from './components/FileRenderer/Renderer';
 import {initAction, setDirectory} from './state/folders/actions';
 import ConfirmationWindow from './components/ConfirmationModal/ConfirmationWindow';
-import { Folder, File } from './state/folders/types';
+import {Folder, File, FileEntity} from './state/folders/types';
+import * as API from './api/index';
 
 
 const Container = styled.div`
@@ -16,36 +18,68 @@ const Container = styled.div`
   flex-direction: row;
 `;
 
-export default function Dashboard() {
+export default function Dashboard(this: any) {
   const [modalState, setModalState] = useState<{open: boolean, type: string}>({
     open: false,
     type: "",
   });
+
+  const [directory, setDirectory] = useState<Array<string>>(["hello", "hi"]);
   const [selectedFile, setSelectedFile] = useState<number|null>(null);
   const dispatch = useDispatch();
+
+  const updateDirectory = (action: string) => {
+    if (action == 'forward') {
+      setDirectory([...directory, "hi"]);
+    } else{
+      setDirectory(directory.slice(0, -1));
+    }
+  };
+
   useEffect(() => {
     // fetches all folders and files from backend and displays it
     dispatch(initAction());
-    dispatch(setDirectory(
-      {
-        path: 'hello/hi',
-        items: [],
-      }
-    ));
+    // dispatch(setDirectory(
+    //   {
+    //     path: '',
+    //     items: [],
+    //   }
+    // ));
   },[])
+
+  // update the directory whenever a folder is being clicked
+  useEffect(() => {
+    const updateDir = async () => {
+      try {
+        const fileInfo: FileEntity = await API.getFolder((selectedFile != null) ? selectedFile : undefined);
+        if (fileInfo.type == 'Folder') {
+          setDirectory([...directory, "hi"]);
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    updateDir();
+  }, [selectedFile])
 
   return (
     <Container>
       <SideBar setModalState={setModalState}/>
       <div>
-        <button>go back</button>
-        <button>forward</button>
-      </div>
+        <button onClick={() => updateDirectory('backward')}>go back</button>
         <Breadcrumbs aria-label="breadcrumb">
-          <Link underline="hover" color="inherit" href="/">
-            MUI
-          </Link>
+          {
+            directory.map((folder, i) => {
+              return (
+                <Link underline="hover" color="inherit" key={i}>
+                  {folder}
+                </Link>
+              )
+            })
+          }
         </Breadcrumbs>
+        <button onClick={() => updateDirectory('forward')}>forward</button>
+      </div>
       <Renderer
         selectedFile={selectedFile}
         setSelectedFile={setSelectedFile}
