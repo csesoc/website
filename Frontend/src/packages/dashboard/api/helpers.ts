dd0100eb08467107b8fcaba246e1024378e26c49
Z5304138 <k.bowers@student.unsw.edu.au>
2022-03-22 10:20:19 +1100

    added helper function to access the foldr state in redux

--- Frontend/src/packages/dashboard/api/helpers.ts
+++ Frontend/src/packages/dashboard/api/helpers.ts
@@ -1,14 +1,24 @@
-import { FileEntity } from "../state/folders/types";
+import {FileEntity, sliceState} from "../state/folders/types";
 import { JSONFileFormat } from "./types";
+import {useSelector} from "react-redux";
+import {RootState} from "../../../redux-state/reducers";
+import {folderSelectors} from "../state/folders";
 
 // Converts a backend response to the File or Folder type
 export function toFileOrFolder(json: JSONFileFormat): FileEntity {
   console.log(json)
-  const {EntityID, EntityName, IsDocument} = json;
+  const {EntityID, EntityName, IsDocument, ParentID} = json;
 
   return {
     id: EntityID,
     name: EntityName,
+    parentId: ParentID,
     type: IsDocument ? "File" : "Folder",
   } as FileEntity
+}
+
+export function getFolderState(): sliceState  {
+  return useSelector((state: RootState) => (
+    folderSelectors.getFolderState(state)
+  ));
 }
