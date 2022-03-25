5a88be9c731a51bfb3e0f3b95c58fcf30545f3dd
leannachan <aypasha23502@gmail.com>
2022-03-26 02:11:24 +0800

    renamed function and made changes based on feedback

--- Frontend/src/cse-ui-kit/dottted_container/dotted_container.stories.tsx
+++ Frontend/src/cse-ui-kit/dottted_container/dotted_container.stories.tsx
@@ -1,33 +1,26 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import Container from './container';
+import DottedContainer from './dotted_container';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: 'CSE-UIKIT/Container',
-  component: Container,
+  title: 'CSE-UIKIT/DottedContainer',
+  component: DottedContainer,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
-} as ComponentMeta<typeof Container>;
+} as ComponentMeta<typeof DottedContainer>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof Container> = (args) =>
+const Template: ComponentStory<typeof DottedContainer> = (args) =>
 (
   <div
     style={{
       margin: "30px"
     }}
   >
-    <Container {...args}></Container>
+    <DottedContainer {...args}></DottedContainer>
   </div>
 )
-
 export const Primary = Template.bind({});
-Primary.args = {
-  background: "#90c2e7",
-}
 
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
