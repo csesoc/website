a4c482faa1632822cc17fe87703e1e3d87f1776f
Jacky Xie <gokurohono@gmail.com>
2022-03-26 16:12:13 +1100

    adding base testing framework

---
+++ Frontend/src/cse-testing-lib/custom-queries.ts
@@ -0,0 +1,32 @@
+import {queryHelpers, buildQueries} from '@testing-library/react'
+
+// The queryAllByAttribute is a shortcut for attribute-based matchers
+// You can also use document.querySelector or a combination of existing
+// testing library utilities to find matching nodes for your query
+
+const queryAllByDataAnchor = (...args: any[]) => {
+  // @ts-ignore
+  return queryHelpers.queryAllByAttribute('data-anchor', ...args)
+}
+
+const getMultipleError = (c: HTMLElement, dataAnchorValue: any) =>
+  `Found multiple elements with the data-anchor attribute of: ${dataAnchorValue}`
+const getMissingError = (c: HTMLElement, dataAnchorValue: any) =>
+  `Unable to find an element with the data-anchor attribute of: ${dataAnchorValue}`
+
+const [
+  queryByDataAnchor,
+  getAllByDataAnchor,
+  getByDataAnchor,
+  findAllByDataAnchor,
+  findByDataAnchor,
+] = buildQueries(queryAllByDataAnchor, getMultipleError, getMissingError)
+
+export {
+  queryByDataAnchor,
+  queryAllByDataAnchor,
+  getByDataAnchor,
+  getAllByDataAnchor,
+  findAllByDataAnchor,
+  findByDataAnchor,
+}
---
+++ Frontend/src/cse-testing-lib/index.ts
@@ -0,0 +1,11 @@
+import {
+  render,
+  fireEvent,
+  act
+} from "./test-utils"
+
+export {
+  render,
+  fireEvent,
+  act
+};
