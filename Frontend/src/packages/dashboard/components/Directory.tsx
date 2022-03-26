ffde6dfea7111f17905b09826f689b841f84557c
Z5304138 <k.bowers@student.unsw.edu.au>
2022-03-26 22:36:01 +1100

    Added styles to breadcrumb

--- Frontend/src/packages/dashboard/components/Directory.tsx
+++ Frontend/src/packages/dashboard/components/Directory.tsx
@@ -1,9 +1,43 @@
-import {Breadcrumbs, Link} from "@mui/material";
 import React from "react";
+import { Breadcrumbs } from "@mui/material";
+import Chip from '@mui/material/Chip';
+import { emphasize, styled as customStyle } from '@mui/material/styles';
+import styled from 'styled-components';
 import { useDispatch } from 'react-redux';
 
 import {traverseBackFolder} from "../state/folders/actions";
 import {getFolderState} from "../api/helpers";
+
+
+const DirectoryFlex = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 20px;
+  align-items: center;
+  justify-content: flex-start;
+  margin: 10px 20px;
+  height: fit-content;
+`
+
+const BreadcrumbItem = customStyle(Chip)(({ theme }) => {
+  const backgroundColor =
+    theme.palette.mode === 'light'
+      ? theme.palette.grey[100]
+      : theme.palette.grey[800];
+  return {
+    backgroundColor,
+    height: theme.spacing(3),
+    color: theme.palette.text.primary,
+    fontWeight: theme.typography.fontWeightRegular,
+    '&:hover, &:focus': {
+      backgroundColor: emphasize(backgroundColor, 0.06),
+    },
+    '&:active': {
+      boxShadow: theme.shadows[1],
+      backgroundColor: emphasize(backgroundColor, 0.12),
+    },
+  };
+});
 
 
 export default function Directory() {
@@ -15,19 +49,20 @@ export default function Directory() {
   };
 
   return (
-    <div>
+    <DirectoryFlex>
       <button onClick={ () => handleClick()}>go back</button>
       <Breadcrumbs aria-label="breadcrumb">
         {
           getFolderState().path.split("/").map((folder, i) => {
             return (
-              <Link underline="hover" color="inherit" key={i}>
-                {folder}
-              </Link>
+              <BreadcrumbItem
+                key={i}
+                label={folder}
+              />
             )
           })
         }
       </Breadcrumbs>
-    </div>
+    </DirectoryFlex>
   )
 }
