3c7ca242c47b3963fc073e844ca3aef475bf70d4
leannachan <aypasha23502@gmail.com>
2022-03-27 00:41:17 +0800

    removed comments in storybook

--- Frontend/src/cse-ui-kit/dottted_container/dotted_container.stories.tsx
+++ Frontend/src/cse-ui-kit/dottted_container/dotted_container.stories.tsx
@@ -3,14 +3,12 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import DottedContainer from './dotted_container';
 
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: 'CSE-UIKIT/DottedContainer',
   component: DottedContainer,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+
 } as ComponentMeta<typeof DottedContainer>;
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: ComponentStory<typeof DottedContainer> = (args) =>
 (
   <div
@@ -22,5 +20,3 @@ const Template: ComponentStory<typeof DottedContainer> = (args) =>
   </div>
 )
 export const Primary = Template.bind({});
-
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
