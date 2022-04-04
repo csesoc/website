b637b943c707e6c7fb81fc4d59bdc09bfe3aff53
Zi-ji <zhuzhijie1003@outlook.com>
2022-04-04 13:07:14 +1000

    Next.js boilerplate setup

--- Next/src/pages/_app.tsx
+++ Next/src/pages/_app.tsx
@@ -1,8 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { ThemeProvider } from "styled-components";
+
+import { theme } from "../styles/theme";
+import { GlobalStyles } from "../styles/globalStyles";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
