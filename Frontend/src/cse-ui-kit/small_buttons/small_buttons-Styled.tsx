a148e34b346d6864240a87be9d5e113b7f8dd1c8
Zi-ji <zhuzhijie1003@outlook.com>
2022-04-11 23:27:22 +1000

    implemented adding multiple content blocks

--- Frontend/src/cse-ui-kit/small_buttons/small_buttons-Styled.tsx
+++ Frontend/src/cse-ui-kit/small_buttons/small_buttons-Styled.tsx
@@ -3,30 +3,30 @@ import styled from "styled-components";
 export const scaleRate = {
   smallButtonRate: 0.8,
   underlineRate: 0.6,
-}
+};
 
 export type buttonProps = {
   background?: string;
   size: number;
-}
+};
+
 export const StyledButton = styled.div<buttonProps>`
-  height: ${props => props.size}px;
-  width: ${props => props.size}px;
-  background: ${props => props.background};
+  height: ${(props) => props.size}px;
+  width: ${(props) => props.size}px;
+  background: ${(props) => props.background};
   color: #000000;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   display: flex;
   justify-content: center;
   align-items: center;
-  border-radius: ${props => props.size / 10}px;
+  border-radius: ${(props) => props.size / 10}px;
 
   &:hover {
-    background: #EFEEF3;
+    background: #efeef3;
     transform: scale(1.04);
   }
   &:active {
-    background: #C8D1FA;
-    color: #7482CB;
+    background: #c8d1fa;
     transform: scale(0.96);
   }
 
--- Frontend/src/packages/editor/components/EditorBlock.tsx
+++ Frontend/src/packages/editor/components/EditorBlock.tsx
@@ -1,18 +1,22 @@
 // Import React dependencies.
-import React, { useState, FC } from "react";
+import React, { FC, MouseEventHandler, useMemo, useCallback } from "react";
 import styled from "styled-components";
 // Import the Slate editor factory.
 import { createEditor, Descendant } from "slate";
 
 // Import the Slate components and React plugin.
-import { Slate, Editable, withReact } from "slate-react";
+import { Slate, Editable, withReact, RenderElementProps } from "slate-react";
+
+import { UpdateValues } from "..";
 
 const EditorContainer = styled.div`
-  width: 500px;
-  height: 200px;
+  width: 100%;
+  max-width: 500px;
+  min-height: 100px;
   display: flex;
   border-radius: 10px;
   margin: 5px;
+  padding: 10px;
   box-shadow: 0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1);
 `;
 
@@ -25,12 +29,36 @@ const initialValues: Descendant[] = [
   },
 ];
 
-const EditorBlock: FC = () => {
-  const [editor] = useState(() => withReact(createEditor()));
+const EditorBlock: FC<{ update: UpdateValues; id: number }> = ({
+  update,
+  id,
+}) => {
+  const editor = useMemo(() => withReact(createEditor()), []);
+
+  const preventTriple: MouseEventHandler<HTMLDivElement> = (event) => {
+    if (event.detail > 2) {
+      event.preventDefault();
+    }
+  };
+
+  const renderElements: (props: RenderElementProps) => JSX.Element =
+    useCallback((props) => {
+      // eslint-disable-next-line react/prop-types
+      return <span {...props.attributes}>{props.children}</span>;
+    }, []);
+
   return (
     <EditorContainer>
-      <Slate editor={editor} value={initialValues}>
-        <Editable style={{ width: "100%", height: "100%" }} />
+      <Slate
+        editor={editor}
+        value={initialValues}
+        onChange={() => update(id, editor.children)}
+      >
+        <Editable
+          onClick={preventTriple}
+          renderElement={renderElements}
+          style={{ width: "100%", minHeight: "100%" }}
+        />
       </Slate>
     </EditorContainer>
   );
