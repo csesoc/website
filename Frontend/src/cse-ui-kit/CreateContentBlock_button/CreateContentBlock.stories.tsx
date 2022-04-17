f08094769b71668e8123d9313a344bf9ea8a71a7
leannachan <aypasha23502@gmail.com>
2022-04-17 23:44:24 +0800

    removed repeated comments

--- Frontend/src/cse-ui-kit/CreateContentBlock_button/CreateContentBlock.stories.tsx
+++ Frontend/src/cse-ui-kit/CreateContentBlock_button/CreateContentBlock.stories.tsx
@@ -4,17 +4,16 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 import CreateContentBlock from './CreateContentBlock';
 
 import { AiFillEdit } from "react-icons/ai";
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+
 export default {
   title: 'CSE-UIKIT/CreateContentBlockButton',
   component: CreateContentBlock,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+
   argTypes: {
     backgroundColor: { control: 'color' },
   },
 } as ComponentMeta<typeof CreateContentBlock>;
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: ComponentStory<typeof CreateContentBlock> = (args) =>
 (
   <div
@@ -31,5 +30,3 @@ export const Primary = Template.bind({});
 Primary.args = {
   background: "#90c2e7",
 }
-
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
