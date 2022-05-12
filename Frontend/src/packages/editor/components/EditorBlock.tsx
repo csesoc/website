b6f23a51e9f8ea0645ae7754ed73a87d76a88be3
Zi-ji <zhuzhijie1003@outlook.com>
2022-05-12 21:54:13 +1000

    implemented editor

---
+++ Frontend/src/packages/editor/components/EditorBlock.tsx
@@ -0,0 +1,93 @@
+// Import React dependencies.
+import React, { FC, useMemo, useCallback } from "react";
+import styled from "styled-components";
+// Import the Slate editor factory.
+import { createEditor, Descendant } from "slate";
+
+// Import the Slate components and React plugin.
+import { Slate, Editable, withReact, RenderLeafProps } from "slate-react";
+
+import BoldButton from "src/cse-ui-kit/small_buttons/BoldButton";
+import ItalicButton from "src/cse-ui-kit/small_buttons/ItalicButton";
+import UnderlineButton from "src/cse-ui-kit/small_buttons/UnderlineButton";
+import ContentBlock from "../../../cse-ui-kit/contentblock/contentblock-wrapper";
+
+const initialValues: Descendant[] = [
+  {
+    type: "paragraph",
+    children: [{ text: "" }],
+  },
+];
+
+const ToolbarContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding-bottom: 10px;
+`;
+
+const Text = styled.span<{
+  bold: boolean;
+  italic: boolean;
+  underline: boolean;
+}>`
+  font-weight: ${(props) => (props.bold ? 600 : 400)};
+  font-style: ${(props) => (props.italic ? "italic" : "normal")};
+  text-decoration-line: ${(props) => (props.underline ? "underline" : "none")};
+`;
+
+interface EditorBlockProps {
+  update: (id: number, value: Descendant[]) => void;
+  id: number;
+  showToolBar: boolean;
+  onClick: () => void;
+}
+
+const EditorBlock: FC<EditorBlockProps> = ({
+  update,
+  id,
+  showToolBar,
+  onClick,
+}) => {
+  const editor = useMemo(() => withReact(createEditor()), []);
+
+  const renderLeaf: (props: RenderLeafProps) => JSX.Element = useCallback(
+    ({ attributes, children, leaf }) => {
+      return (
+        <Text
+          bold={leaf.bold ?? false}
+          italic={leaf.italic ?? false}
+          underline={leaf.underline ?? false}
+          {...attributes}
+        >
+          {children}
+        </Text>
+      );
+    },
+    []
+  );
+
+  return (
+    <ContentBlock>
+      <Slate
+        editor={editor}
+        value={initialValues}
+        onChange={() => update(id, editor.children)}
+      >
+        {showToolBar && (
+          <ToolbarContainer>
+            <BoldButton size={30} />
+            <ItalicButton size={30} />
+            <UnderlineButton size={30} />
+          </ToolbarContainer>
+        )}
+        <Editable
+          renderLeaf={renderLeaf}
+          onClick={() => onClick()}
+          style={{ width: "100%", height: "100%" }}
+        />
+      </Slate>
+    </ContentBlock>
+  );
+};
+
+export default EditorBlock;
---
+++ Frontend/src/packages/editor/index.tsx
@@ -0,0 +1,68 @@
+import styled from "styled-components";
+import React, { useState, FC, useEffect } from "react";
+import { Descendant } from "slate";
+
+import EditorHeader from "src/deprecated/components/Editor/EditorHeader";
+import EditorBlock from "./components/EditorBlock";
+
+type BlockData = Descendant[];
+export type UpdateValues = (idx: number, updatedBlock: BlockData) => void;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const ButtonsContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  padding: 10px;
+`;
+
+const EditorPage: FC = () => {
+  const [blocks, setBlocks] = useState<BlockData[]>([]);
+  const [focusedId, setFocusedId] = useState<number>(0);
+
+  useEffect(() => {
+    console.log(blocks);
+  }, [blocks]);
+
+  const updateValues: UpdateValues = (idx: number, updatedBlock: BlockData) => {
+    if (JSON.stringify(blocks[idx]) !== JSON.stringify(updateValues)) return;
+    setBlocks((prev) =>
+      prev.map((block, i) => (i === idx ? updatedBlock : block))
+    );
+  };
+
+  return (
+    <div style={{ height: "100%" }}>
+      <EditorHeader />
+      <ButtonsContainer>
+        <button
+          style={{ userSelect: "none" }}
+          onClick={() => {
+            setBlocks((prev) => [...prev, []]);
+            setFocusedId(blocks.length);
+          }}
+        >
+          add block
+        </button>
+      </ButtonsContainer>
+      <Container>
+        {blocks.map((_, idx) => (
+          <EditorBlock
+            onClick={() => setFocusedId(idx)}
+            showToolBar={focusedId === idx}
+            key={idx}
+            id={idx}
+            update={updateValues}
+          />
+        ))}
+      </Container>
+    </div>
+  );
+};
+
+export default EditorPage;
