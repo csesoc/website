2d58775b8bcb11ce3615d19a43886b020c730e31
Zi-ji <zhuzhijie1003@outlook.com>
2022-05-16 13:38:10 +1000

    improve prop types for CreateContentBlock

--- Frontend/src/cse-ui-kit/CreateContentBlock_button/CreateContentBlock.tsx
+++ Frontend/src/cse-ui-kit/CreateContentBlock_button/CreateContentBlock.tsx
@@ -1,18 +1,15 @@
-import React from 'react';
-import { StyledButton, buttonProps } from './CreateContentBlock-Styled';
+import React, { MouseEventHandler } from "react";
+import { StyledButton, buttonProps } from "./CreateContentBlock-Styled";
 import { AiFillEdit } from "react-icons/ai";
 
 type Props = {
-  onClick?: (...args: any) => void;
+  onClick?: MouseEventHandler<HTMLDivElement>;
 } & buttonProps;
 
-export default function CreateContentBlock({onClick, ...styleProps }: Props) {
+export default function CreateContentBlock({ onClick, ...styleProps }: Props) {
   return (
-    <StyledButton
-      onClick={onClick}
-      {...styleProps}
-    >
-      <AiFillEdit/>
+    <StyledButton onClick={onClick} {...styleProps}>
+      <AiFillEdit />
       Insert Content Block
     </StyledButton>
   );
