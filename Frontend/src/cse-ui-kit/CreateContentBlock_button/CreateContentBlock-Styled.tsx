999dc33ec5392e72430c4022510412c9a32387ab
Zhijie Zhu <53923477+zijizhu@users.noreply.github.com>
2022-05-17 22:48:26 +1000

    [CMS-183] Editor implementation using slate.js (#122)
    * install slate
    
    * implemented editor
    
    * minor bug fixes
    
    * removed draft-js
    
    * folder structure change, implemented small buttons
    
    * clean up
    
    * clean up
    
    * improve prop types for CreateContentBlock
    
    * added comments
    
    * moved buttons outside the content block

--- Frontend/src/cse-ui-kit/CreateContentBlock_button/CreateContentBlock-Styled.tsx
+++ Frontend/src/cse-ui-kit/CreateContentBlock_button/CreateContentBlock-Styled.tsx
@@ -2,26 +2,26 @@ import styled from "styled-components";
 
 export type buttonProps = {
   background?: string;
-}
+};
 export const StyledButton = styled.div<buttonProps>`
   width: 175px;
   height: 45px;
-  background: ${props => props.background };
-  color: white;
+  margin: 5px;
+  background: ${(props) => props.background};
 
   display: flex;
   justify-content: center;
   align-items: center;
   border-radius: 10px;
+  user-select: none;
 
   &:hover {
-    background: #EFEEF3;
+    background: #efeef3;
     color: black;
     transform: scale(1.04);
   }
   &:active {
-    background: #C8D1FA;
-    color: #7482CB;
+    background: #c8d1fa;
     transform: scale(0.96);
   }
 
