10dd2f47c1b01419a75b334f69b68395b19b634b
Zi-ji <zhuzhijie1003@outlook.com>
2022-05-22 16:43:21 +1000

    fixed bugs

--- Next/src/pages/index.tsx
+++ Next/src/pages/index.tsx
@@ -1,8 +1,8 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from 'next/image';
+import Image from "next/image";
 import styled from "styled-components";
-import HomepageIcon from './assets/HomepageIcon';
+import HomepageIcon from "../components/assets/HomepageIcon";
 
 const PageContainer = styled.div`
   min-height: 100vh;
@@ -13,11 +13,9 @@ const PageContainer = styled.div`
 `;
 
 const ImageContainer = styled.div`
-  @import url('https://fonts.googleapis.com/css?family=Raleway:300,400,700&display=swap');
   position: absolute;
   left: 10%;
   top: 25%;
-  font-family: "Raleway", sans-serif;
   justify-content: flex-end;
 `;
 
@@ -30,7 +28,7 @@ const Text1 = styled.body`
 `;
 
 const Text2 = styled.body`
-  color: #3977F8;
+  color: #3977f8;
   font-size: 36px;
 `;
 
@@ -38,16 +36,16 @@ const Text3 = styled.body`
   color: #010033;
   font-style: italic;
   font-size: 36px;
-  display:inline;
+  display: inline;
 `;
 
 const Button = styled.button`
-  background-color:#FFFFFF;
-  color: #3977F8;
+  background-color: #ffffff;
+  color: #3977f8;
   font-size: 22px;
   margin-top: 150px;
   padding: 0.25em 1em;
-  border: 1px solid #3977F8;
+  border: 1px solid #3977f8;
   border-radius: 3px;
   position: absolute;
   width: 184px;
@@ -66,7 +64,7 @@ const Home: NextPage = () => {
       <main>
         <HomepageIcon />
         <ImageContainer>
-          <Image src="/logo.svg" width="362" height="84" />
+          <Image src="/logo.svg" width="362" height="84" alt="csesoc logo" />
           <Text1>
             Empowering
             <Text3> future</Text3>
@@ -82,4 +80,3 @@ const Home: NextPage = () => {
 };
 
 export default Home;
-
