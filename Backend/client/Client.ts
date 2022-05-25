d4368a2208cdad1d4c796b9dceca42949ecd3a7a
Gary Sun <garysun0451@gmail.com>
2022-05-26 02:51:18 +1000

    Add document id to constructor and handle ack timeout

--- Backend/client/Client.ts
+++ Backend/client/Client.ts
@@ -1,29 +1,31 @@
 /**
  * Client server implementation
- * example payload = {
- *   "type": "ack" | "op",
- *   "data": {
- *   }
- * }
  */
 
-import { io } from "socket.io-client";
+import { io, Socket } from "socket.io-client";
 import Operation from "./Operation";
 
+const ACK_TIMEOUT_DURATION = 10_000;
+
 export default class Client {
-  socket = io("ws://localhost:8080");
+  socket: Socket;
   state = null;
   isAcked = true;
   opID = 0;
+  timeoutID: number = NaN;
   buffer: Operation[] = [];
 
-  constructor() {
+  constructor(document: string) {
+    this.socket = io(`ws://localhost:8080/edit?document=${document}`);
+
     this.socket.on("connect", () => {
       console.log(`Socket ${this.socket.id} connected: ${this.socket.connected}`);
     });
 
     this.socket.on("ack", () => {
+      clearTimeout(this.timeoutID);
       this.isAcked = true;
+
       // Send next packet if there is one
       const operation = this.buffer.shift();
       if (operation !== undefined) {
@@ -44,11 +46,14 @@ export default class Client {
    */
   public send(operation: Operation) {
     if (this.isAcked) {
-      // Send the message and check that we get an ACK back
+      // Send the message
       this.socket.send(JSON.stringify(operation));
       this.isAcked = false;
+      this.timeoutID = setTimeout(() => {
+        throw new Error(`Did not receive ACK after ${ACK_TIMEOUT_DURATION} ms!`)
+      }, ACK_TIMEOUT_DURATION, "finish")
     } else {
-      // Add to buffers
+      // Add to buffer
       this.buffer.push(operation);
     }
   }
