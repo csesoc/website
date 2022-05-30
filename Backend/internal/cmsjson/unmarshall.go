368322738f692937d29171b7ba2e5e43e9bd7da6
Varun Sethu <varun.sethu@outlook.com>
2022-05-30 19:59:44 +1000

    added marshaller

--- Backend/internal/cmsjson/unmarshall.go
+++ Backend/internal/cmsjson/unmarshall.go
@@ -1,22 +1,10 @@
-package marshaller
-
-// What is this? Well the default go marshaller does not support interface types
-// this is a partial custom implementation that adds the support for interface types
-// accepted types are defined in the RegisteredType field in the configuration
-// for the most part this is just a bunch of reflection bashing + using a better json parser
+package cmsjson
 
 import (
 	"reflect"
 
 	"github.com/tidwall/gjson"
 )
-
-type Configuration struct {
-	// registeredTypes maps registered interface types to their implementation
-	// forcing registration restricts the set of types we can marshall into thus
-	// improving security
-	RegisteredTypes map[reflect.Type]map[string]reflect.Type
-}
 
 // unmarshall some json into a destination struct, the idea behind this
 // is that we use reflection to iterate over the members of dest and match them
@@ -120,31 +108,4 @@ func (c Configuration) parsePrimitive(result gjson.Result, expected reflect.Type
 	// This is in order to deal with typecasts between integers
 	// namely ints and int64s
 	return reflect.ValueOf(value).Convert(expected)
-}
-
-type typeCategory int
-
-const (
-	_primitive = iota
-	_interface
-	_struct
-	_array
-	_slice
-)
-
-// resolveType takes a reflection field and determines what "type category" it falls into
-// we have differing logic for differing type categories
-func resolveType(t reflect.Type) typeCategory {
-	switch t.Kind() {
-	case reflect.Struct:
-		return _struct
-	case reflect.Interface:
-		return _interface
-	case reflect.Slice:
-		return _slice
-	case reflect.Array:
-		return _array
-	}
-
-	return _primitive
 }
