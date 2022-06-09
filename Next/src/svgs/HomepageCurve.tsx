b91b61765e281929290ff08f47155286080ea661
Z5304138 <k.bowers@student.unsw.edu.au>
2022-06-09 18:01:26 +1000

    minor changes: "stop-color" -> "stopColor"; "stop-opacity" -> stopOpacity

--- Next/src/svgs/HomepageCurve.tsx
+++ Next/src/svgs/HomepageCurve.tsx
@@ -13,8 +13,8 @@ export default function HomepageCurve({width, height}: Props) {
       <path d="M17 457.5C155.5 155.5 529.329 68.5207 604 0L604 1658C466 1614.5 58.5789 1491.73 155.5 1339.5C254.5 1184 546.602 1139.6 477.5 1006C440 933.5 -99.5972 711.741 17 457.5Z" fill="url(#paint0_linear_494_154)"/>
       <defs>
       <linearGradient id="paint0_linear_494_154" x1="337.5" y1="0" x2="337.5" y2="1658" gradientUnits="userSpaceOnUse">
-      <stop stop-color="#8F8DDB" stop-opacity="0"/>
-      <stop offset="1" stop-color="#8F8DDB"/>
+      <stop stopColor="#8F8DDB" stopOpacity="0"/>
+      <stop offset="1" stopColor="#8F8DDB"/>
       </linearGradient>
       </defs>
     </svg>
