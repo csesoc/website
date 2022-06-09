8f6c5a9de934b909d576729b2904402ca7a0017e
Jacky Xie <47090372+fafnirZ@users.noreply.github.com>
2022-06-09 16:18:32 +1000

    [CMS-271] Create swirl curvecomponents in figma and import into homepage (#135)
    * styled nextjs homepage
    
    * added global colors to stylesheet
    
    * added Rectangle curve svg too
    
    * minor styling fixes

--- Next/src/pages/index.tsx
+++ Next/src/pages/index.tsx
@@ -1,9 +1,17 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import styled from "styled-components";
-import Events from './Events'
+
+// local
 import Homepage from "./Homepage";
+import Events from './Events'
 import AboutUs from './AboutUs';
+import HomepageCurve from "../svgs/HomepageCurve";
+import RectangleCurve from "../svgs/RectangleCurve";
+
+type CurveContainerProps = {
+  offset: number;
+}
 
 const PageContainer = styled.div`
   min-height: 100vh;
@@ -12,6 +20,13 @@ const PageContainer = styled.div`
   padding-left: 2rem;
   padding-right: 2rem;
 `;
+
+const CurveContainer = styled.div<CurveContainerProps>`
+  position: absolute;
+  top: ${props => props.offset}px;
+  right: 0;
+`;
+
 
 // const Button = styled.button`
 //   background-color:#FFFFFF;
@@ -27,6 +42,7 @@ const PageContainer = styled.div`
 // `;
 
 const Home: NextPage = () => {
+
   return (
     <PageContainer>
       <Head>
@@ -35,6 +51,15 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
+        <CurveContainer offset={0}>
+          <HomepageCurve width={400} height={1000}/>
+        </CurveContainer>
+        <CurveContainer offset={1000}>
+          <RectangleCurve
+            height={1000}
+            dontPreserveAspectRatio
+          />
+        </CurveContainer>
         <Homepage />
         <AboutUs />
         <Events />
