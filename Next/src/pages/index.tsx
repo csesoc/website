c8c0642deb30250ec980a42e03cb716c9de93aaa
Z5304138 <k.bowers@student.unsw.edu.au>
2022-06-10 17:54:00 +1000

    changed name for consistency

--- Next/src/pages/index.tsx
+++ Next/src/pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from 'next/image';
-import Navbar from "../components/Navbar"
+import Navbar from "../components/navbar"
 
 import styled from "styled-components";
 
