763aaf5bfc13aaa5d51c676c9fd5eaddb153e2e9
Z5304138 <k.bowers@student.unsw.edu.au>
2022-06-10 18:33:48 +1000

    deleted incorrect import

--- Next/src/pages/index.tsx
+++ Next/src/pages/index.tsx
@@ -1,7 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from 'next/image';
-import Navbar from "../components/navbar"
 
 import styled from "styled-components";
 
