bc9a56f80213aa3b7521f80b3fc59cb37d6c9f6c
Brian Zhang <brian.zhang2101@gmail.com>
2022-06-12 16:01:36 +1000

    added edit stubs

--- Backend/editor/models/datamodel.go
+++ Backend/editor/models/datamodel.go
@@ -1,13 +1,5 @@
 package models
 
-import (
-	"encoding/json"
-	"errors"
-	"reflect"
-	"strconv"
-	"strings"
-)
-
 type Request struct {
 	path    string  `json:"path"`
 	op      string  `json:"op"`
@@ -25,105 +17,58 @@ type Document struct {
 // - Make sure the item we are adding keeps the validity of the object
 
 func process(request string) (err error) {
-	requestObject := Request{}
-	if err := json.Unmarshal([]byte(request), &requestObject); err != nil {
-		return errors.New("Invalid request format")
-	}
-	switch editType := requestObject.payload.GetType(); editType {
-	case "textEdit":
-		break
-	case "keyEdit":
-		break
-	case "arrayEdit":
-		break
-	default:
-		return errors.New("Invalid edit type")
-	}
 	return nil
 }
 
 // Parses a string path into the starting index of content, subpaths to reach said object
 func pathParser(path string) ([]string, error) {
-	subpaths := strings.Split(path, "/")
-	// TODO: Maybe generalise this hardcoded check
-	if len(subpaths) < 1 || subpaths[0] != "content" {
-		return nil, errors.New("First subpath must be 'content'")
-	}
-	return subpaths, nil
+	return nil, nil
 }
 
 // Converts the data string into the correct data type
 func dataTypeEvaluator(dataStr string, dataType string) (data interface{}, err error) {
-	switch dataType {
-	case "integer":
-		if result, err := strconv.ParseInt(dataStr, 10, 32); err != nil {
-			return nil, errors.New("Data is not an integer")
-		} else {
-			return result, nil
-		}
-	case "boolean":
-		if result, err := strconv.ParseBool(dataStr); err != nil {
-			return nil, errors.New("Data is not a boolean")
-		} else {
-			return result, nil
-		}
-	case "float":
-		if result, err := strconv.ParseFloat(dataStr, 64); err != nil {
-			return nil, errors.New("Data is not a float")
-		} else {
-			return result, nil
-		}
-	case "string":
-		return dataStr, nil
-	case "component":
-		var result Component
-		if err := config.Unmarshall([]byte(dataStr), &result); err != nil {
-			return nil, err
-		} else {
-			return result, nil
-		}
-	default:
-		return nil, errors.New("Incompatible data type")
-	}
+	return nil, nil
 }
 
-// TODO: Error check index logic
+// Gets the data at the end of the input path
 func (d Document) get(path string) (interface{}, error) {
-	paths, err := pathParser(path)
-	if err != nil {
-		return nil, err
-	}
-	// We get the parent struct/array
-	target := paths[len(paths)-1]
-	var parentPointer reflect.Value
-	parentPointer = traverse(d, paths)
-	// Check if we are dealing with an array/slice
-	if parentPointer.Kind() == reflect.Array || parentPointer.Kind() == reflect.Slice {
-		// Target must thus be an index if len(paths) > 1
-		if len(paths) > 1 {
-			if index, err := strconv.ParseInt(target, 10, 32); err != nil {
-				return nil, err
-			} else {
-				return parentPointer.Elem().Index(int(index)), nil
-			}
-		} else { // Edge case for a path "content" -> we return everything inside it
-			return parentPointer.Elem(), nil
-		}
-	}
-	// We are dealing with a struct
-	for i := 0; i < parentPointer.NumField(); i++ {
-		field := parentPointer.Field(i)
-		if field.Type().Field(i).Name == target {
-			return field.Elem(), nil
-		}
-	}
-	// Didn't find the key
-	return nil, errors.New("Didn't find")
+	return nil, nil
 }
 
-// TODO
 // textEdit functions
+
+// Add update text field
+func (d Document) textEditUpdate(path string, start int, end int, data string) error {
+	return nil
+}
+
+// Remove text field
+func (d Document) textEditRemove(path string, start int, end int) error {
+	return nil
+}
 
 // keyEdit functions
 
+// Add data field
+func (d Document) keyEditInsert(path string, data interface{}) error {
+	return nil
+}
+
+// Remove data field
+func (d Document) keyEditRemove(path string) error {
+	return nil
+}
+
 // arrayEdit functions
+
+// TODO: do we want to maintain the length too to reduce time complexity?
+
+// Update element in array at "index" position
+func (d Document) arrayEditUpdate(path string, index int, data interface{}) error {
+	return nil
+}
+
+// Remove element in array at "index" position
+func (d Document) arrayEditRemove(path string, index int) error {
+	return nil
+}
