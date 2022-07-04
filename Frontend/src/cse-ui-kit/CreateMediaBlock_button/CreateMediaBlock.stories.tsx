5a263810bf8e2e994090f74b7bc6ac33577df6a8
oodwin <edwin@IdeaPad-S540.localdomain>
2022-07-04 15:28:12 +1000

    Created CreateMediaBlock button

---
+++ Frontend/src/cse-ui-kit/CreateMediaBlock_button/CreateMediaBlock.stories.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import CreateMediaBlock from './CreateMediaBlock';
+
+import { AiFillPicture } from "react-icons/ai";
+
+export default {
+  title: 'CSE-UIKIT/CreateMediaBlockButton',
+  component: CreateMediaBlock,
+
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as ComponentMeta<typeof CreateMediaBlock>;
+
+const Template: ComponentStory<typeof CreateMediaBlock> = (args) =>
+(
+  <div
+    style={{
+      margin: "30px"
+    }}
+  >
+    Insert Button
+    <CreateMediaBlock {...args}></CreateMediaBlock>
+  </div>
+)
+
+export const Primary = Template.bind({});
+Primary.args = {
+  background: "#90c2e7",
+}
