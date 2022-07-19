d3829240e0fcc70012d32616893c0a748a5e2f89
Laura <laurawen003@gmail.com>
2022-07-19 13:01:59 +1000

    Added popup functionality

--- Frontend/src/cse-ui-kit/contentblock_popup/contentblock_popup.stories.tsx
+++ Frontend/src/cse-ui-kit/contentblock_popup/contentblock_popup.stories.tsx
@@ -1,12 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-
+import Button from '@mui/material/Button';
+import Modal from '@mui/material/Modal';
+import Backdrop from '@mui/material/Backdrop';
+import Fade from '@mui/material/Fade';
 import ContentBlockPopup from './contentblock_popup';
 
 export default {
   title: 'CSE-UIKIT/UploadContentPopup',
   component: ContentBlockPopup,
 } as ComponentMeta<typeof ContentBlockPopup>;
+
+
+function Func() {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  return (
+    <div>
+      <Button onClick={handleOpen}>Open modal</Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: 'transparent' }}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          invisible: true,
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <div>
+            <ContentBlockPopup />
+          </div>
+        </Fade>
+      </Modal>
+    </div>
+  )
+}
 
 const Template: ComponentStory<typeof ContentBlockPopup> = (args) =>
 (
@@ -16,7 +48,7 @@ const Template: ComponentStory<typeof ContentBlockPopup> = (args) =>
     }}
   >
     UploadContentPopup
-    <ContentBlockPopup />
+    <Func />
   </div>
 )
 
--- Frontend/src/cse-ui-kit/contentblock_popup/contentblock_popup.tsx
+++ Frontend/src/cse-ui-kit/contentblock_popup/contentblock_popup.tsx
@@ -1,4 +1,6 @@
+/* eslint-disable */
 import React from 'react';
+
 import {
   StyledContainer,
   StyledDottedContainer,
