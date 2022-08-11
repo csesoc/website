32b907b4539656347e4d0bc0b326676d5b4e91b4
Zhijie Zhu <53923477+zijizhu@users.noreply.github.com>
2022-08-11 23:42:54 +1000

    [CMS-275] Editor Publish Button (#169)
    * websocket wip
    
    * ws client wip
    
    * minor fixes
    
    * publish button wip
    
    * fixed backend sending content issues
    
    * fixed package dependency issue with react-refresh
    
    * Fixed State Sending from Backend (#168)
    
    * fixed backend sending content issues
    
    * fixed package dependency issue with react-refresh
    
    * implemented and tested publish button
    
    * end to end connection
    
    Co-authored-by: Varun Sethu <varun.sethu@outlook.com>
    Co-authored-by: Brian Zhang <brian.zhang2101@gmail.com>
    Co-authored-by: Brian Zhang <32604762+brianzhang2101@users.noreply.github.com>
    Co-authored-by: Jacky <gokurohono@gmail.com>

--- Frontend/src/packages/editor/components/EditorBlock.tsx
+++ Frontend/src/packages/editor/components/EditorBlock.tsx
@@ -1,16 +1,22 @@
 import styled from "styled-components";
 import { createEditor, Descendant } from "slate";
 import React, { FC, useMemo, useCallback } from "react";
-import { Slate, Editable, withReact, RenderLeafProps, useSlate } from "slate-react";
+import {
+  Slate,
+  Editable,
+  withReact,
+  RenderLeafProps,
+  useSlate,
+} from "slate-react";
 
-import { UpdateHandler } from "../types";
+import { BlockData, UpdateHandler } from "../types";
 import EditorBoldButton from "./buttons/EditorBoldButton";
 import EditorItalicButton from "./buttons/EditorItalicButton";
 import EditorUnderlineButton from "./buttons/EditorUnderlineButton";
-import EditorSelectFont from './buttons/EditorSelectFont'
-import EditorCenterAlignButton from './buttons/EditorCenterAlignButton'
-import EditorLeftAlignButton from './buttons/EditorLeftAlignButton'
-import EditorRightAlignButton from './buttons/EditorRightAlignButton'
+import EditorSelectFont from "./buttons/EditorSelectFont";
+import EditorCenterAlignButton from "./buttons/EditorCenterAlignButton";
+import EditorLeftAlignButton from "./buttons/EditorLeftAlignButton";
+import EditorRightAlignButton from "./buttons/EditorRightAlignButton";
 
 import ContentBlock from "../../../cse-ui-kit/contentblock/contentblock-wrapper";
 import { toggleMark, handleKey } from "./buttons/buttonHelpers";
@@ -18,7 +24,7 @@ import { getBlockContent } from "../state/helpers";
 
 // Redux
 import { useDispatch } from "react-redux";
-import {updateContent} from "../state/actions";
+import { updateContent } from "../state/actions";
 
 const defaultTextSize = 16;
 
@@ -39,15 +45,16 @@ const Text = styled.span<{
 }>`
   font-weight: ${(props) => (props.bold ? 600 : 400)};
   font-style: ${(props) => (props.italic ? "italic" : "normal")};
-  font-size: ${(props) => (props.textSize)}px;
+  font-size: ${(props) => props.textSize}px;
   text-decoration-line: ${(props) => (props.underline ? "underline" : "none")};
-  text-align: ${(props) => (props.align)}; 
+  text-align: ${(props) => props.align};
 `;
 
-const AlignedText = Text.withComponent('div')
+const AlignedText = Text.withComponent("div");
 
 interface EditorBlockProps {
   update: UpdateHandler;
+  initialValue: BlockData;
   id: number;
   showToolBar: boolean;
   onEditorClick: () => void;
@@ -56,30 +63,29 @@ interface EditorBlockProps {
 const EditorBlock: FC<EditorBlockProps> = ({
   id,
   update,
+  initialValue,
   showToolBar,
   onEditorClick,
 }) => {
-
-  const dispatch  = useDispatch();
+  const dispatch = useDispatch();
   const editor = useMemo(() => withReact(createEditor()), []);
 
   const renderLeaf: (props: RenderLeafProps) => JSX.Element = useCallback(
     ({ attributes, children, leaf }) => {
-      return (
-        leaf.align == null ? 
-          <Text
-            // Nullish coalescing operator
-            // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Nullish_coalescing_operator
-            bold={leaf.bold ?? false}
-            italic={leaf.italic ?? false}
-            underline={leaf.underline ?? false}
-            textSize={leaf.textSize ?? 16}
-            align={leaf.align ?? "left"}
-            {...attributes}
-          >
-            {children}
-          </Text>
-      :
+      return leaf.align == null ? (
+        <Text
+          // Nullish coalescing operator
+          // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Nullish_coalescing_operator
+          bold={leaf.bold ?? false}
+          italic={leaf.italic ?? false}
+          underline={leaf.underline ?? false}
+          textSize={leaf.textSize ?? 16}
+          align={leaf.align ?? "left"}
+          {...attributes}
+        >
+          {children}
+        </Text>
+      ) : (
         <AlignedText
           // Nullish coalescing operator
           // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Nullish_coalescing_operator
@@ -92,24 +98,26 @@ const EditorBlock: FC<EditorBlockProps> = ({
         >
           {children}
         </AlignedText>
-      )
+      );
     },
     []
   );
 
-  const initialValue = getBlockContent(id);
+  // const initialValue = getBlockContent(id);
 
   return (
     <Slate
       editor={editor}
       value={initialValue}
       onChange={(value) => {
-        update(id, editor.children)
+        update(id, editor.children);
 
-        dispatch(updateContent({
-          id: id,
-          data: value,
-        }))
+        dispatch(
+          updateContent({
+            id: id,
+            data: value,
+          })
+        );
       }}
     >
       {showToolBar && (
@@ -127,7 +135,7 @@ const EditorBlock: FC<EditorBlockProps> = ({
         <Editable
           renderLeaf={renderLeaf}
           onClick={() => onEditorClick()}
-          style={{ width: "100%", height: "100%"}}
+          style={{ width: "100%", height: "100%" }}
           onKeyDown={(event) => handleKey(event, editor)}
         />
       </ContentBlock>
