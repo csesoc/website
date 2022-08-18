4d5ab989d49451b74c0cce48137431d9272f8308
Varun Sethu <varun.sethu@outlook.com>
2022-08-19 01:13:36 +1000

    Ghost Exporter - Parser (#181)
    * added ghost export format parser
    
    * removed rage comments
    
    * added cms marshaller
    
    * fixed gitignore
    
    * fixed gitignore

---
+++ utilities/ghost-exporter/Main.fs
@@ -0,0 +1,23 @@
+﻿module Main
+
+open GhostSyntax
+open Newtonsoft.Json
+
+open FSharpPlus
+open Fleece.Newtonsoft
+open Fleece.Newtonsoft.Operators
+open System.IO
+open Converters
+
+[<EntryPoint>]
+let main args =
+    let ghostDocument: GhostDocument ParseResult = ofJsonText (File.ReadAllText args[0])
+
+    let result = 
+        match Option.ofResult ghostDocument with
+        | Some document -> toJsonText (GhostToCms document) 
+        | _ -> "failed to parse ghost document"
+
+    printfn "%s" result
+    
+    0
