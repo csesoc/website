a802510b016fbe899714432ae95228aa7cfbf3f9
Varun Sethu <varun.sethu@outlook.com>
2022-08-18 18:31:43 +1000

    added ghost export format parser

---
+++ utilities/ghost-exporter/Main.fs
@@ -0,0 +1,18 @@
+﻿module Main
+
+open GhostSyntax
+open Newtonsoft.Json
+
+[<EntryPoint>]
+let main args =
+    printfn "%A" (JsonConvert.DeserializeObject("""
+    [
+        "a",
+        [
+          "href",
+          "https://en.wikipedia.org/wiki/Composite_pattern"
+        ]
+    ]
+    """))
+
+    0
