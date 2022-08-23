a4a7cf706df696805ab7dfedc254d2605858676f
Brian Zhang <brian.zhang2101@gmail.com>
2022-08-24 01:37:44 +1000

    added fixed indice paths

--- backend/editor/OT/data/applicator.go
+++ backend/editor/OT/data/applicator.go
@@ -9,14 +9,11 @@ import (
 	"cms.csesoc.unsw.edu.au/editor/OT/data/datamodels"
 )
 
-// TODO:
-// - Add error checking for the paths as we traverse, e.g missing an index when traversing an array (assuming we didn't reach the end)
-// - Make sure the item we are adding keeps the validity of the object
-
 // ApplyRequest takes a datamodel (as defined in the datamodels folder) and a request, it then proceeds to apply the request
 // to the model, note that this assumes that the operation in the request has been appropriately transformed
 func ApplyRequest(model datamodels.DataModel, request OperationRequest) error {
-	_, err := GetOperationTargetSite(model, request.Path)
+	// TODO: Use Gary's code here to get the indice path
+	_, err := GetOperationTargetSite(model, []int{})
 	if err != nil {
 		return err
 	}
@@ -34,7 +31,7 @@ func ApplyRequest(model datamodels.DataModel, request OperationRequest) error {
 
 // getOperationTargetSite Gets the target object at the end of the path, this is the operation that we need to apply
 // our operation to
-func GetOperationTargetSite(model datamodels.DataModel, subpaths []string) (reflect.Value, error) {
+func GetOperationTargetSite(model datamodels.DataModel, subpaths []int) (reflect.Value, error) {
 	_, target, err := Traverse(model, subpaths)
 	if err != nil {
 		return reflect.Value{}, err
