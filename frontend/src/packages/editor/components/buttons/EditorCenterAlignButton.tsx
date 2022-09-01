4f67fce92cfcf27b206bf2bc2d2ae73e9ff35c27
James <74297507+james-teng-0@users.noreply.github.com>
2022-09-01 21:52:52 +1000

    sidebar, alignment and style updates (#211)
    * add sidebar, alignment and style updates
    
    * Fix sidebar tests
    
    * style collapse button
    
    Co-authored-by: James <james.teng@csesoc.org.au>

--- frontend/src/packages/editor/components/buttons/EditorCenterAlignButton.tsx
+++ frontend/src/packages/editor/components/buttons/EditorCenterAlignButton.tsx
@@ -2,11 +2,13 @@ import React, { FC } from "react";
 import { useSlate } from "slate-react";
 import { toggleMark } from "./buttonHelpers";
 import { Editor } from "slate";
+import CenterAlignButton from "src/cse-ui-kit/small_buttons/CenterAlignButton";
 
 const EditorCenterAlignButton: FC = () => {
   const editor = useSlate();
   return (
-    <button
+    <CenterAlignButton
+      size={30}
       onMouseDown={(event) => {
         event.preventDefault();
         Editor.addMark(editor, "align", "center")
