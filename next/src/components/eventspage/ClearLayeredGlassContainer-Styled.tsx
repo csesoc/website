83963214488308a237de39babafe4b5b7672593d
Jacky Xie <gokurohono@gmail.com>
2022-09-14 22:27:18 +1000

    fix(next): glass container mobile

--- next/src/components/eventspage/ClearLayeredGlassContainer-Styled.tsx
+++ next/src/components/eventspage/ClearLayeredGlassContainer-Styled.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { device } from "../../styles/device";
 
 export type positionProps = {
     position: string;
@@ -9,8 +10,7 @@ export type positionProps = {
 
 export const GlassContainer = styled.div<positionProps>`
     position: ${(props) => props.position};
-    width: 36.7vw;
-    height: 20vw;
+
     top: ${(props) => props.top}vw;
     left: ${(props) => props.left}vw;
     border-radius: 1vw;
@@ -18,12 +18,24 @@ export const GlassContainer = styled.div<positionProps>`
     border-width: 0.15vw;
     border-style: solid;
     border-color: #FAFCFF;
+    width: 80vw;
+    height: 50vw;
+    @media ${device.tablet} {
+        width: 36.7vw;
+        height: 20vw;
+    }
 `;
 
 export const ImgContainer = styled.div`
     position: relative;
-    width: 36vw;
-    height: 17.8vw;
-    top: 2.15vw;
+    width: 77vw;
+    height: 60vw;
+    top: -4.15vw;
     left: 1.5vw;
+    @media ${device.tablet} {
+        width: 36vw;
+        height: 17.8vw;
+        top: 2.15vw;
+        left: 1.5vw;
+    }
 `
