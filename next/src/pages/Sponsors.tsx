34709f816f6e5ff3407ee836cc94081a8b3e88a0
Laura <laurawen003@gmail.com>
2022-09-16 00:21:12 +1000

    Removed unnecessary code

--- next/src/pages/Sponsors.tsx
+++ next/src/pages/Sponsors.tsx
@@ -1,15 +1,8 @@
 import React from "react";
 import Image from "next/image";
 import { content } from "../assets/sponsors.js";
-import defaultCardImage from "../assets/sponsors/ak.webp";
 
-// Fetching data from the JSON file
-import fsPromises from 'fs/promises';
-import path from 'path'
 export const getStaticProps = async () => {
-  // const filePath = path.join(process.cwd(), '/root/cse/uni/cms.csesoc.unsw.edu.au/next/src/assets/sponsors.json');
-  // const jsonData = await fsPromises.readFile(filePath);
-  // const objectData = JSON.parse(jsonData);
   return {
     props: {
       objectData: content,
@@ -38,46 +31,23 @@ export default function Sponsors(objectData: Array<SponsorTypes>) {
       <p>
         Principal Sponsors
       </p>
-      <div>
-      {
-      
-          <div
-            key={content.id}
-            style={{
-              padding: 20, borderBottom: '1px solid #ccc'
-            }}
-          >
-            {/* <Image
-              src={`../assets/sponsors/"${objectData.logo}"`}
-              layout="fill"
-              objectFit="contain"
-            /> */}
-            {/* <Image
-                src={`/assets/sponsors/${+ content.logo}}`}
-                layout="fill"
-                objectFit="contain"
-              /> */}
-            {
-          content.map((Sponsor) =>
-            <div
-              key={Sponsor.id}
-              style={{
-                padding: 20, borderBottom: '1px solid #ccc'
-              }}
-            >
-              {Sponsor.alt_text}
-              <Image
-                src={`/assets/sponsors/${Sponsor.logo}`}
-                width="100px"
-                height="100px"
-              />
-              
-            </div>
-          )}
-          </div>
-      
-        }
-      </div>
+      {content.map((Sponsor) => {
+
+        <div
+          key={Sponsor.id}
+          style={{
+            padding: 20, borderBottom: '1px solid #ccc'
+          }}
+        >
+          {Sponsor.alt_text}
+          <Image
+            src={`/assets/sponsors/${Sponsor.logo}`}
+            width="100px"
+            height="100px"
+          />
+
+        </div>
+      })}
       <p>
         Major Sponsors
       </p>
