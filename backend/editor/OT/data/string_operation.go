f193d3ead69a94412758a47b0a666015b39575d7
Gary Sun <garysun0451@gmail.com>
2022-09-17 02:42:02 +1000

    Complete non overlapping text transform functions

--- backend/editor/OT/data/string_operation.go
+++ backend/editor/OT/data/string_operation.go
@@ -7,12 +7,12 @@ import (
 // StringOperation represents an operation on a string type
 // @implements OperationModel
 type StringOperation struct {
-	rangeStart, rangeEnd int
-	newValue             string
+	RangeStart, RangeEnd int
+	NewValue             string
 }
 
-// If newValue == "", delete every character between rangeStart and rangeEnd
-// Else rangeEnd = rangeStart + len(newValue), and insert newValue starting at rangeStart
+// If NewValue == "", delete every character between RangeStart and RangeEnd
+// Else RangeEnd = RangeStart + len(NewValue), and insert NewValue starting at RangeStart
 
 // TransformAgainst is the StringOperation implementation of the operationModel interface
 func (stringOp StringOperation) TransformAgainst(operation OperationModel, applicationType EditType) (OperationModel, OperationModel) {
@@ -20,7 +20,7 @@ func (stringOp StringOperation) TransformAgainst(operation OperationModel, appli
 	if !ok {
 		return stringOp, operation
 	}
-	if othStringOp.newValue != "" {
+	if othStringOp.NewValue != "" {
 		if applicationType == Insert {
 			return insertInsert(othStringOp, stringOp), insertInsert(stringOp, othStringOp)
 		} else {
@@ -34,7 +34,7 @@ func (stringOp StringOperation) TransformAgainst(operation OperationModel, appli
 		}
 	}
 	// If the operation to transform it against is not a StringOperation, do nothing
-	// Else transform it by adjusting rangeStart / rangeEnd values to account for other edit
+	// Else transform it by adjusting RangeStart / RangeEnd values to account for other edit
 	// https://srijancse.medium.com/operational-transformation-the-real-time-collaborative-editing-algorithm-bf8756683f66
 	// Then return (transformedOperation, operation)
 }
@@ -46,54 +46,54 @@ func (stringOp StringOperation) Apply(parentNode cmsjson.AstNode, applicationInd
 
 // Transform o2 when o1 is an insert, and o2 is a insert
 func insertInsert(o1 StringOperation, o2 StringOperation) StringOperation {
-	if o1.rangeStart >= o2.rangeStart {
+	if o1.RangeStart >= o2.RangeStart {
 		// If other insert happens before our insert, shift insert right
-		length := o2.rangeEnd - o2.rangeStart
-		o1.rangeStart, o1.rangeEnd = o1.rangeStart+length, o1.rangeEnd+length
+		length := o2.RangeEnd - o2.RangeStart
+		o1.RangeStart, o1.RangeEnd = o1.RangeStart+length, o1.RangeEnd+length
 	}
 	return o1
 }
 
 // Transform o1 when o1 is an insert, and o2 is a delete
 func insertDelete(o1 StringOperation, o2 StringOperation) StringOperation {
-	if o1.rangeStart <= o2.rangeStart {
+	if o1.RangeStart <= o2.RangeStart {
 		// If insert happens before the delete, do nothing to insert op
 		return o1
-	} else if o1.rangeStart >= o2.rangeEnd {
+	} else if o1.RangeStart >= o2.RangeEnd {
 		// If the insert happens after the delete, shift insert left
-		length := o2.rangeEnd - o2.rangeStart
-		o1.rangeStart, o1.rangeEnd = o1.rangeStart-length, o1.rangeEnd-length
+		length := o2.RangeEnd - o2.RangeStart
+		o1.RangeStart, o1.RangeEnd = o1.RangeStart-length, o1.RangeEnd-length
 	} else {
 		// Overlap case FUCK FUCK FUCK
-		o1.rangeStart = o2.rangeStart
+		o1.RangeStart = o2.RangeStart
 	}
 	return o1
 }
 
 // Transform o1 when o1 is an delete, and o2 is a insert
 func deleteInsert(o1 StringOperation, o2 StringOperation) StringOperation {
-	if o2.rangeStart < o1.rangeEnd {
+	if o2.RangeStart < o1.RangeEnd {
 		// If delete happens after the insert, do nothing to delete op
 		return o1
-	} else if o1.rangeStart >= o2.rangeStart {
+	} else if o1.RangeStart >= o2.RangeStart {
 		// If delete happens after insert, shift delete right
-		length := o2.rangeEnd - o2.rangeStart
-		o1.rangeStart, o1.rangeEnd = o1.rangeStart+length, o1.rangeEnd+length
+		length := o2.RangeEnd - o2.RangeStart
+		o1.RangeStart, o1.RangeEnd = o1.RangeStart+length, o1.RangeEnd+length
 	}
-	if o1.rangeEnd < o2.rangeStart {
+	if o1.RangeEnd < o2.RangeStart {
 		// Overlap case FUCK FUCK FUCK
-		o1.rangeStart = o2.rangeStart
+		o1.RangeStart = o2.RangeStart
 	}
 	return o1
 }
 
 // Transform o2 when o1 is an delete, and o2 is a delete
 func deleteDelete(o1 StringOperation, o2 StringOperation) StringOperation {
-	if o2.rangeStart >= o1.rangeEnd {
+	if o2.RangeStart >= o1.RangeEnd {
 		return o1
-	} else if o1.rangeStart >= o2.rangeEnd {
-		length := o2.rangeEnd - o2.rangeStart
-		o1.rangeStart, o2.rangeEnd = o1.rangeStart+length, o1.rangeEnd+length
+	} else if o1.RangeStart >= o2.RangeEnd {
+		length := o2.RangeEnd - o2.RangeStart
+		o1.RangeStart, o2.RangeEnd = o1.RangeStart+length, o1.RangeEnd+length
 	} else {
 		// Overlap case FUCK FUCK FUCK
 	}
---
+++ backend/editor/OT/data/tests/string_operation_test.go
@@ -0,0 +1,65 @@
+package tests
+
+import (
+	"log"
+	"math"
+	"testing"
+
+	"cms.csesoc.unsw.edu.au/editor/OT/data"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestInsertInsertNonOverlap(t *testing.T) {
+	str := "abcde"
+	o1 := data.StringOperation{RangeStart: 1, RangeEnd: 2, NewValue: "1"}
+	o2 := data.StringOperation{RangeStart: 2, RangeEnd: 3, NewValue: "2"}
+	o1_t, o2_t := o1.TransformAgainst(o2, data.Insert)
+
+	assert := assert.New(t)
+	assert.True(apply(o1_t, apply(o2_t, str)) == apply(o2_t, apply(o1_t, str)))
+}
+
+func TestInsertInsertOverlap(t *testing.T) {
+	str := "abcde"
+	o1 := data.StringOperation{RangeStart: 1, RangeEnd: 3, NewValue: "11"}
+	o2 := data.StringOperation{RangeStart: 2, RangeEnd: 4, NewValue: "22"}
+	o1_t, o2_t := o1.TransformAgainst(o2, data.Insert)
+
+	assert := assert.New(t)
+	assert.True(consistent(str, o1_t, o2_t))
+}
+
+func TestInsertDeleteNonOverlap(t *testing.T) {
+	str := "abcde"
+	o1 := data.StringOperation{RangeStart: 1, RangeEnd: 2, NewValue: "1"}
+	o2 := data.StringOperation{RangeStart: 2, RangeEnd: 3, NewValue: ""}
+	o1_t, o2_t := o1.TransformAgainst(o2, data.Delete)
+
+	assert := assert.New(t)
+	assert.True(consistent(str, o1_t, o2_t))
+}
+
+func TestDeleteDeleteNonOverlap(t *testing.T) {
+	str := "abcde"
+	o1 := data.StringOperation{RangeStart: 1, RangeEnd: 2, NewValue: ""}
+	o2 := data.StringOperation{RangeStart: 2, RangeEnd: 3, NewValue: ""}
+	o1_t, o2_t := o1.TransformAgainst(o2, data.Delete)
+
+	assert := assert.New(t)
+	assert.True(consistent(str, o1_t, o2_t))
+}
+
+// Return if two operations result in same output regardless of order applied
+func consistent(s string, o1 data.OperationModel, o2 data.OperationModel) bool {
+	return apply(o1, apply(o2, s)) == apply(o2, apply(o1, s))
+}
+
+// Apply an operational model to a string if it is a string operation else fail
+func apply(o data.OperationModel, s string) string {
+	so, ok := o.(data.StringOperation)
+	if !ok {
+		log.Fatalf("Failed to convert operation to string operation")
+	}
+	end := int(math.Min(float64(len(s)), float64(so.RangeEnd)))
+	return (s[:so.RangeStart] + so.NewValue + s[end:])
+}
