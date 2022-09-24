a6da325561027950225470f7073713e6401480a7
Aslyui <alex.ljchen520@gmail.com>
2022-09-24 21:53:00 +1000

    i think this works :eyes:

--- utilities/ghost-exporter/Converters.fs
+++ utilities/ghost-exporter/Converters.fs
@@ -5,51 +5,116 @@ open GhostSyntax
 open GhostSyntax.Sections
 open GhostSyntax.Cards
 
+let findTextStyle (markups: Markups.markup list) (markup: Markups.markup) : bool = 
+    List.exists (fun m -> m = markup) markups
 
-let convertMarkup (markUps: Markups.markup list) : textStyle = 
+let convertMarkups (markups: Markups.markup list) : textStyle = 
     {
-        bold = List.exists (fun markup -> match markup with | Markups.Link _ -> true | _ -> false) markUps
-        italic = List.exists (fun markup -> match markup with | Markups.Code _ -> true | _ -> false) markUps
-        underline = List.exists (fun markup -> match markup with | Markups.Emphasis _ -> true | _ -> false) markUps
+        bold = findTextStyle markups Markups.Strong // Strong seems like a direct translation for bold despite also having a bold markup?
+        italic = findTextStyle markups Markups.Emphasis // Emphasis seems like a direct translation with italics despite there being an italics markup?
+        underline = false
     }
 
+// This needs to be implemented properly once frontend is ready
+let convertCallout (callout: Cards.callout) : text list = 
+    [{
+        data = Text callout.calloutText
+        style = {
+            bold = false
+            italic = false
+            underline = false
+        }
+    }]
+
+// This needs to be implemented properly once frontend is ready
+let convertToggle (toggle: Cards.toggle) : text list = 
+    [{
+        data = Text toggle.heading
+        style = {
+            bold = true
+            italic = false
+            underline = false
+        }
+    };
+    {
+        data = Text toggle.content
+        style = {
+            bold = true
+            italic = false
+            underline = false
+        }
+    }]
+
+let convertCode (code: Cards.code) : text list = 
+    [{
+        data = Text code.code
+        style = {
+            bold = false
+            italic = false
+            underline = false
+        }
+    }]
 
 let convertCard (cards: Cards.card list) (cardIndex: int) : text list = 
     match List.tryItem cardIndex cards with
             | Option.None -> []
             | Some value -> match value with
-                            | Callout callout -> convertCallout Callout
-                            | Toggle toggle -> convertToggle Toggle
-                            | Code code -> convertCode Code
+                            | Callout callout -> convertCallout callout
+                            | Toggle toggle -> convertToggle toggle
+                            | Code code -> convertCode code
 
 let modifyOpenMarkups (openMarkups: int list) (newMarkups: int list) (numClosedMarkups: int) : int list = 
-    // How does adding and removing markups actually work in ghost?
     let rec removeMarkups (markups: int list, numClosedMarkups: int) : int list = 
         match numClosedMarkups with
         | 0 -> markups
         | _ -> removeMarkups (List.tail markups, numClosedMarkups - 1)
-    // My current assumption is that we append newMarkups to the list and close the last markup in the list
+    // My current assumption is that we append newMarkups to the list and remove starting from the last markup in the list
     removeMarkups (List.append openMarkups newMarkups, numClosedMarkups)
 
-let convertSectionTexts (markups: Markups.markup list) (sectionBlock: list<sectionBlock>) : text list = 
-    let rec aux (sections: list<sectionBlock>) (openMarkups : int list): text list = 
-        match sections with
+let retrieveMarkups (markups: Markups.markup list) (indices: int list) : Markups.markup list = 
+    List.map (fun index -> List.item index markups) indices
+
+
+ 
+let convertValue (sectionBlock : sectionBlock) (openMarkups : Markups.markup list) : textType = 
+    let isLink (markup : Markups.markup) : bool = 
+        match markup with
+            | Markups.Link _ -> true 
+            | _ -> false
+    
+    let listContainsLink (list : Markups.markup list) : bool = 
+        List.exists (isLink) list
+
+    if listContainsLink openMarkups then
+        Text sectionBlock.value // Should be URL type in future
+    else
+        Text sectionBlock.value
+
+
+let convertSectionBlocks (markups: Markups.markup list) (sectionBlock: list<sectionBlock>) : text list = 
+    let rec convertSectionBlock (sections: list<sectionBlock>) (openMarkups : int list) : text list = 
+        match sections with 
             | [] -> []
             | x :: xs -> 
-                let markup = modifyOpenMarkups openMarkups x.openMarkups x.numClosedMarkups
-                {
-                    data = Text x.value
-                    style = List.map (fun x -> List.tryItem x markups) markup |> convertMarkups
-                } :: (aux (xs) markup)
+                let openMarkups = modifyOpenMarkups openMarkups x.openMarkups x.numClosedMarkups
+                let retrievedMarkups = retrieveMarkups markups openMarkups
+                let isLink = function | Markups.Link _ -> true | _ -> false
 
-    aux sectionBlock []
+                {
+                    data = convertValue x retrievedMarkups
+                    style = convertMarkups retrievedMarkups
+
+                } :: (convertSectionBlock xs openMarkups)
+    
+    convertSectionBlock sectionBlock []
+
+
 let convertSection (cards: Cards.card list) (markups: Markups.markup list) (section: Sections.section) : paragraph = 
     {
         paragraphAllign = Center
         children = match section.blocks with
-                    | Section sectionBlock -> convertSectionTexts markups sectionBlock
+                    | Section sectionBlock -> convertSectionBlocks markups sectionBlock
                     | CardReference index -> convertCard cards index
-                    | _ -> []
     }
 
 
