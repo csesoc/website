edebd2497ad2c51ca8cfe7ebed551f3f3609942e
Varun Sethu <varun.sethu@outlook.com>
2022-09-24 17:55:29 +1000

    pair coding

--- utilities/ghost-exporter/Converters.fs
+++ utilities/ghost-exporter/Converters.fs
@@ -1,40 +1,26 @@
 module Converters
-module List
 
 open CMSSyntax
 open GhostSyntax
+open GhostSyntax.Sections
+open GhostSyntax.Cards
 
-let GhostToCms (document: GhostDocument) : CMSDocument = 
+
+let convertMarkup (markUps: Markups.markup list) : textStyle = 
     {
-        documentName = "replace me"
-        documentId = 0
-        content = List.Map (convertSection document.Cards document.Markups) document.content
+        bold = List.exists (fun markup -> match markup with | Markups.Link _ -> true | _ -> false) markUps
+        italic = List.exists (fun markup -> match markup with | Markups.Code _ -> true | _ -> false) markUps
+        underline = List.exists (fun markup -> match markup with | Markups.Emphasis _ -> true | _ -> false) markUps
     }
 
-let convertSection (cards: Cards.card list) (markups: Markups.markup list) (section: Sections.section) : paragraph = 
-    let texts = match section.blockValue with
-                | Section sectionBlock -> convertSectionTexts markups sectionBlock
-                | CardReference index -> convertCard cards index
-                | _ -> []
-    {
-        paragraphAllign = Center
-        children = texts
-    }
 
-let convertSectionTexts (markups: Markups.markup list) (sectionBlock: sectionBlock) : text list = 
-    // Some loop that iterates over sectionBlock and keeps track of each markup this feels ugly to me
-    // There has to be some sort of functional way to do this? probs with map fold or monads idfk
-    texts = []
-    openMarkups = []
-    for section in sectionBlock do
-        let markup = modifyOpenMarkups openMarkups section.openMarkups section.numClosedMarkups
-        let text = {
-            data = sectionBlock.value
-            style = List.Map (fun x -> List.tryItem x markups) markups |> convertMarkups
-        }
-        texts = texts :: text
-    // Hopefully this is the way to return a value?
-    texts
+let convertCard (cards: Cards.card list) (cardIndex: int) : text list = 
+    match List.tryItem cardIndex cards with
+            | Option.None -> []
+            | Some value -> match value with
+                            | Callout callout -> convertCallout Callout
+                            | Toggle toggle -> convertToggle Toggle
+                            | Code code -> convertCode Code
 
 let modifyOpenMarkups (openMarkups: int list) (newMarkups: int list) (numClosedMarkups: int) : int list = 
     // How does adding and removing markups actually work in ghost?
@@ -43,42 +29,34 @@ let modifyOpenMarkups (openMarkups: int list) (newMarkups: int list) (numClosedM
         | 0 -> markups
         | _ -> removeMarkups (List.tail markups, numClosedMarkups - 1)
     // My current assumption is that we append newMarkups to the list and close the last markup in the list
-    let newMarkups = removeMarkups (openMarkups :: newMarkups, numClosedMarkups)
-    newMarkups
+    removeMarkups (List.append openMarkups newMarkups, numClosedMarkups)
 
-let convertMarkup (markUps: Markups.markup list) : style = 
-    link = False
-    code = False
-    emphasis = False
-    strong = False
+let convertSectionTexts (markups: Markups.markup list) (sectionBlock: list<sectionBlock>) : text list = 
+    let rec aux (sections: list<sectionBlock>) (openMarkups : int list): text list = 
+        match sections with
+            | [] -> []
+            | x :: xs -> 
+                let markup = modifyOpenMarkups openMarkups x.openMarkups x.numClosedMarkups
+                {
+                    data = Text x.value
+                    style = List.map (fun x -> List.tryItem x markups) markup |> convertMarkups
+                } :: (aux (xs) markup)
 
-    for markup in markUps do
-        match markup with
-        | Markups.Link _ -> link = True
-        | Markups.Code _ -> code = True
-        | Markups.Emphasis _ -> emphasis = True
-        | Markups.Strong _ -> strong = True
+    aux sectionBlock []
+let convertSection (cards: Cards.card list) (markups: Markups.markup list) (section: Sections.section) : paragraph = 
     {
-        link = link
-        code = code
-        emphasis = emphasis
-        strong = strong
+        paragraphAllign = Center
+        children = match section.blocks with
+                    | Section sectionBlock -> convertSectionTexts markups sectionBlock
+                    | CardReference index -> convertCard cards index
+                    | _ -> []
     }
 
 
-let convertCard (cards: Cards.card list, cardIndex: int) : text list = 
-    cardToConvert = List.tryItem cardIndex cards
-    let texts = match cardToConvert with
-                | callout Callout -> convertCallout Callout
-                | toggle Toggle -> convertToggle Toggle
-                | code Code -> convertCode Code
-                | _ -> None
-    
-    texts
-
-
-let convertCallout (callout: callout) : text list = 
-    // How do we actually convert callouts to text?
-    // I'm assuming we just return the text of the callout and ignore colour and emojir for now
-    Text callout.calloutText
+let GhostToCms (document: GhostDocument) : CMSDocument = 
+    {
+        documentName = "replace me"
+        documentId = 0
+        content = List.map (convertSection document.Cards document.Markups) document.Sections
+    }
 
