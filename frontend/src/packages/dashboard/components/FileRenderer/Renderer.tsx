4102d0d5313cf05ebae2891181f35bcbaab17829
Zhijie Zhu <53923477+zijizhu@users.noreply.github.com>
2022-09-24 14:50:16 +1000

    [Cms-300-new] Document UUID and WebSocket connection fixes (#251)
    * adapted feteching to uuid
    
    * fixing edtor endpoint
    
    * changes
    
    * debugging
    
    * replaced magic numbers with constants
    
    Co-authored-by: Varun Sethu <varun.sethu@outlook.com>

--- frontend/src/packages/dashboard/components/FileRenderer/Renderer.tsx
+++ frontend/src/packages/dashboard/components/FileRenderer/Renderer.tsx
@@ -1,33 +1,27 @@
-import React from 'react'
-import { useSelector } from 'react-redux';
-import { RootState } from 'src/redux-state/reducers';
-import { folderSelectors } from '../../state/folders/index';
-import FileContainer from './FileContainer';
-import FolderContainer from './FolderContainer';
+import React from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "src/redux-state/reducers";
+import { folderSelectors } from "../../state/folders/index";
+import FileContainer from "./FileContainer";
+import FolderContainer from "./FolderContainer";
 
 type Props = {
-  selectedFile: number | null;
-  setSelectedFile: (id: number) => void;
-}
+  selectedFile: string | null;
+  setSelectedFile: (id: string) => void;
+};
 
-export default function Renderer({ selectedFile, setSelectedFile }: Props){
-  const folders = useSelector((state: RootState) => (
+export default function Renderer({ selectedFile, setSelectedFile }: Props) {
+  const folders = useSelector((state: RootState) =>
     folderSelectors.getFolderState(state)
-  ));
+  );
 
   const folderItems = folders.items;
-  
-  const renderItems = () => (
+
+  const renderItems = () =>
     folderItems.map((item, index) => {
-      switch(item.type) {
+      switch (item.type) {
         case "Folder":
-          return (
-            <FolderContainer 
-              key={index}
-              id={item.id}
-              name={item.name}
-            />
-          )
+          return <FolderContainer key={index} id={item.id} name={item.name} />;
         case "File":
           return (
             <FileContainer
@@ -37,18 +31,18 @@ export default function Renderer({ selectedFile, setSelectedFile }: Props){
               selectedFile={selectedFile}
               setSelectedFile={setSelectedFile}
             />
-          )
+          );
         default:
           return;
       }
-    })
-  )
+    });
   return (
-    <div style={{
-      display: "flex",
-    }}>
+    <div
+      style={{
+        display: "flex",
+      }}
+    >
       {renderItems()}
     </div>
-  )
+  );
 }
-
