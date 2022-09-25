e5f077c15602809fc9b23a5b12dc09597ecdbe4c
Zhijie Zhu <53923477+zijizhu@users.noreply.github.com>
2022-09-25 14:00:25 +1000

    [CMS-301] Closing WebSocket properly when going back and tab close (#254)
    * adapted feteching to uuid
    
    * fixing edtor endpoint
    
    * changes
    
    * debugging
    
    * replaced magic numbers with constants
    
    * trying to fix editor
    
    * hacky fix
    
    * added callback for tab close
    
    Co-authored-by: Varun Sethu <varun.sethu@outlook.com>

--- backend/endpoints/main.go
+++ backend/endpoints/main.go
@@ -33,6 +33,7 @@ type (
 		FormType    string
 		Handler     func(form T, dependencyFactory DependencyFactory) (response handlerResponse[V])
 		IsMultipart bool
+		IsWebsocket bool
 	}
 
 	// rawHandler is a handler that expect the incoming w and r request objects
@@ -42,6 +43,7 @@ type (
 		Handler     func(form T, w http.ResponseWriter, r *http.Request, dependencyFactory DependencyFactory) (response handlerResponse[V])
 		IsMultipart bool
 		NeedsAuth   bool
+		IsWebsocket bool
 	}
 
 	// authenticatedHandler is basically a regular http handler the only difference is that
@@ -81,7 +83,10 @@ func (fn handler[T, V]) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	response := fn.Handler(*parsedForm, dependencyFactory)
 
 	// Record and write out any useful information
-	writeResponse(w, response)
+	if !fn.IsWebsocket {
+		writeResponse(w, response)
+	}
+
 	logResponse(logger, response)
 	logger.Close()
 }
