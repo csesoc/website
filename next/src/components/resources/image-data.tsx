ed5d29d1e5e87105491d21dd2717f22461706903
James <jamest.1402@gmail.com>
2022-09-25 23:55:03 +1000

    add animations for carousel

---
+++ next/src/components/resources/image-data.tsx
@@ -0,0 +1,21 @@
+import degree_planner from "../../svgs/degree_planner.svg"
+import jobs_board from "../../svgs/jobs_board.svg"
+import notangles from "../../svgs/notangles.svg"
+
+const images = [
+    {
+        url: degree_planner.src,
+        link: "https://www.google.com/"
+    },
+    {
+        url: jobs_board.src,
+        link: "https://www.google.com/"
+    },
+    {
+        url: notangles.src,
+        link: "https://www.google.com/"
+    }
+
+];
+
+export default images;
