b480fcac6184782c4b834372be371259fb71416f
Zi-ji <zhuzhijie1003@outlook.com>
2022-09-26 21:52:22 +1000

    updating blog data type

--- next/src/pages/blog/index.tsx
+++ next/src/pages/blog/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 import Blog from "../../components/blog/Blog";
 import Link from "next/link";
 import { data } from "../../mock";
-import type { Element } from "../../components/blog/types";
+import type { Block } from "../../components/blog/types";
 import { useEffect, useState } from "react";
 import styled from "styled-components";
 
@@ -21,18 +21,18 @@ const MainContainer = styled.div`
 `;
 
 const BlogPage: NextPage = () => {
-  const [paragraph, setParagraph] = useState<Element[][]>([]);
+  const [paragraph, setParagraph] = useState<Block[]>([]);
 
   useEffect(() => {
     async function fetchData() {
       const data = await fetch(
-        `${process.env.NEXT_PUBLIC_BACKEND_URI}/api/filesystem/get/published?DocumentID=7`,
+        `${process.env.NEXT_PUBLIC_BACKEND_URI}/api/filesystem/get/published?DocumentID=e643b5bd-4867-4e9f-911b-40885161ae42`,
         {
           method: "GET",
         }
       ).then((res) => res.json());
       console.log(JSON.parse(data.Response.Contents));
-      setParagraph(JSON.parse(data.Response.Contents) as Element[][]);
+      setParagraph(JSON.parse(data.Response.Contents) as Block[]);
     }
     fetchData();
   }, []);
@@ -41,7 +41,7 @@ const BlogPage: NextPage = () => {
     <PageContainer>
       <MainContainer>
         <h1>blog1</h1>
-        <Blog elements={paragraph} />
+        <Blog blocks={paragraph} />
         <Link href="/">home</Link>
       </MainContainer>
     </PageContainer>
