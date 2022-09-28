a91b183ff52ad0bfe47db0956583f0b2d2be1e0c
Zi-ji <zhuzhijie1003@outlook.com>
2022-09-28 22:54:08 +1000

    Fixed blog rendering issues

--- next/src/components/blog/Blog-styled.tsx
+++ next/src/components/blog/Blog-styled.tsx
@@ -2,15 +2,15 @@ import styled from "styled-components";
 import { TextStyle } from "./types";
 import { device } from "../../styles/device";
 
-interface ParagraphStyle {
-  align?: "left" | "right" | "center";
-}
-
 const Text = styled.span<TextStyle>`
   font-weight: ${(props) => (props.bold ? 600 : 400)};
   font-style: ${(props) => (props.italic ? "italic" : "normal")};
   text-decoration-line: ${(props) => (props.underline ? "underline" : "none")};
+  text-align: ${(props) => props.align};
+  font-size: ${(props) => `${props.textSize}px` ?? "16px"};
 `;
+
+const AlignedText = Text.withComponent("div");
 
 const StyledLink = styled.span`
   color: red;
@@ -22,13 +22,11 @@ const ImagePlaceholder = styled.div`
   background-color: #5a6978;
 `;
 
-const ParagraphBlock = styled.p<ParagraphStyle>`
-  // text-align: ${(props) => props.align ?? "left"};
-  text-align: left;
+const ParagraphContainer = styled.div`
+  padding: 10px;
 `;
 
 const BlogContainer = styled.div`
-  
   font-size: 1.25rem;
   margin: 0px 60px;
 
@@ -39,15 +37,20 @@ const BlogContainer = styled.div`
   @media (min-width: 1920px) {
     max-width: 1440px;
   }
-
-
 `;
 
-const BlogHeading = styled.span<TextStyle>`
+const BlogHeading = styled.span`
   font-weight: 800;
   font-size: 35px;
   padding: 20px 0px;
+`;
 
-`
-
-export { Text, StyledLink, ImagePlaceholder, ParagraphBlock, BlogContainer, BlogHeading };
+export {
+  Text,
+  StyledLink,
+  AlignedText,
+  ImagePlaceholder,
+  BlogContainer,
+  BlogHeading,
+  ParagraphContainer,
+};
--- next/src/components/blog/Blog.tsx
+++ next/src/components/blog/Blog.tsx
@@ -2,9 +2,9 @@ import Link from "next/link";
 
 import {
   Text,
-  StyledLink,
+  ParagraphContainer,
   ImagePlaceholder,
-  ParagraphBlock,
+  AlignedText,
   BlogContainer,
 } from "./Blog-styled";
 import type { Element, Block } from "./types";
@@ -14,23 +14,21 @@ const Block = ({ element }: { element: Element }) => {
     return <ImagePlaceholder>{element.url}</ImagePlaceholder>;
   }
   return (
-    <ParagraphBlock align={element.align}>
+    <ParagraphContainer>
       {element.children.map(({ text, link, ...textStyle }, idx) => (
-        <Text key={idx} {...textStyle}>
-          {/* if link attribute is undefined, the current node is plain text */}
-          {/* if link attribute is string, the curent node is a hyper link, with url link */}
-          {link ? (
-            <StyledLink>
-              <Link href={link} passHref>
-                {text}
-              </Link>
-            </StyledLink>
+        <>
+          {textStyle.align === undefined || textStyle.align === "left" ? (
+            <Text key={idx} {...textStyle}>
+              {text}
+            </Text>
           ) : (
-            <>{text}</>
+            <AlignedText key={idx} {...textStyle}>
+              {text}
+            </AlignedText>
           )}
-        </Text>
+        </>
       ))}
-    </ParagraphBlock>
+    </ParagraphContainer>
   );
 };
 
