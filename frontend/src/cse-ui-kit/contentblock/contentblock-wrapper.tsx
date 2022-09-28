872bfd144f265a2ab57008b696d059d5e0494743
Jacky <gokurohono@gmail.com>
2022-09-28 21:39:49 +1000

    feat(cms|editor): ui improvement

--- frontend/src/cse-ui-kit/contentblock/contentblock-wrapper.tsx
+++ frontend/src/cse-ui-kit/contentblock/contentblock-wrapper.tsx
@@ -1,14 +1,16 @@
+/* tslint:disable */
 import React from 'react';
-import { StyledContent, StyledContentDots } from './contentblock-Styled';
+import { StyledContent, StyledContentDots, StyledProps } from './contentblock-Styled';
 import { ReactComponent as Dots } from '../../assets/moveable-content-dots.svg';
 import { Box } from "@mui/material";
 
 type Props = {
   children?: React.ReactElement | any;
   onClick?: (...args: any) => void;
-};
+} & StyledProps;
 
 export default function MoveableContentBlock({ children, onClick, ...styleProps }: Props) {
+  const { focused } = styleProps;
   return (
     <StyledContent
       onClick={onClick}
@@ -24,10 +26,12 @@ export default function MoveableContentBlock({ children, onClick, ...styleProps 
         {children}
       </div>
       <StyledContentDots>
-        <Dots
-          height="18px"
-          width="18px"
-        />
+        { (!!focused) &&
+          <Dots
+            height="18px"
+            width="18px"
+          />
+        }
       </StyledContentDots>
     </StyledContent>
   );
