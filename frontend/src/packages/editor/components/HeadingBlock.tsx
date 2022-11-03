90470b7667dc017ab1f731913fac955f0502d291
Varun Sethu <varun.sethu@outlook.com>
2022-11-04 10:58:22 +1100

    OT Frontend Changes - Stage 1 (#298)
    * Added stage 1 of OT frontend changes - operation capture
    
    * code cleanup + refactor
    
    * trying to resolve broken pipeline
    
    * review changes

--- frontend/src/packages/editor/components/HeadingBlock.tsx
+++ frontend/src/packages/editor/components/HeadingBlock.tsx
@@ -3,15 +3,10 @@ import { createEditor } from "slate";
 import React, { FC, useMemo, useCallback } from "react";
 import { Slate, Editable, withReact, RenderLeafProps } from "slate-react";
 
-import { UpdateHandler } from "../types";
+import { CMSBlockProps } from "../types";
 import EditorSelectFont from './buttons/EditorSelectFont'
 import ContentBlock from "../../../cse-ui-kit/contentblock/contentblock-wrapper";
 import { handleKey } from "./buttons/buttonHelpers";
-import { getBlockContent } from "../state/helpers";
-
-// Redux
-import { useDispatch } from "react-redux";
-import {updateContent} from "../state/actions";
 
 const defaultTextSize = 24;
 
@@ -29,30 +24,21 @@ const Text = styled.span<{
   font-size: ${(props) => (props.textSize)}px;
 `;
 
-interface HeadingBlockProps {
-  update: UpdateHandler;
-  id: number;
-  showToolBar: boolean;
-  onEditorClick: () => void;
-}
 
-const HeadingBlock: FC<HeadingBlockProps> = ({
+const HeadingBlock: FC<CMSBlockProps> = ({
   id,
   update,
   showToolBar,
+  initialValue,
   onEditorClick,
 }) => {
-
-  const dispatch  = useDispatch();
   const editor = useMemo(() => withReact(createEditor()), []);
 
   const renderLeaf: (props: RenderLeafProps) => JSX.Element = useCallback(
     ({ attributes, children, leaf }) => {
       return (
-        <Text
-          // Nullish coalescing operator
-          // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Nullish_coalescing_operator
-          textSize={leaf.textSize ?? defaultTextSize}
+        <Text 
+          textSize={leaf.textSize ?? defaultTextSize} 
           {...attributes}
         >
           {children}
@@ -62,20 +48,11 @@ const HeadingBlock: FC<HeadingBlockProps> = ({
     []
   );
 
-  const initialValue = getBlockContent(id);
-
   return (
     <Slate
       editor={editor}
       value={initialValue}
-      onChange={(value) => {
-        update(id, editor.children)
-
-        dispatch(updateContent({
-          id: id,
-          data: value,
-        }))
-      }}
+      onChange={(value) => { update(id, editor.children, editor.operations); }}
     >
       {showToolBar && (
         <ToolbarContainer>
