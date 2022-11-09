e5e128e09f81962637252b87379a2e753a8da943
Gary Sun <garysun0451@gmail.com>
2022-11-09 14:41:30 +1100

    feat(cms): add string transformation functions (#232)
    * Add non overlapping transformation cases
    
    * Complete non overlapping text transform functions
    
    * Add tests for overlapping text transform cases
    
    OMG TEST DRIVEN DEVELOPMENT
    
    * Complete delete overlapping cases
    
    * Correct delete tests to have expected output
    
    * Correct application function in the tests and comment out DeleteDeleteSame test
    
    * Everything working except for insertdelete overlap delete before insert
    
    * Add copier dependency
    
    * Complete go text transformation implementation and tests
    
    * Add claification comments for tests
    
    * Do not use copier library and remove from dependencies
    
    * Complete go text transformation functions for same location insert
    
    * Correct style and formatting of ts operation file
    
    * Complete ts implementation with tests
    
    * Remove console.logs
    
    * Complete implementation of atomic operation in ts client
    
    * Change interface variable names and add documentation on functions
    
    * Also include the code for applying atomic operations
    
    * Complete tests for path transform implementation
    
    * merging with main
    
    Co-authored-by: Varun Sethu <varun.sethu@outlook.com>

--- backend/editor/OT/OTClient/operation.ts
+++ backend/editor/OT/OTClient/operation.ts
@@ -1,165 +1,184 @@
-// Represents atomic operations that can be applied to a piece of data of a specific type
-// TODO: in the future update object operation to strictly contain CMS operation data
-type stringOperation = { rangeStart: number; rangeEnd: number, newValue: string };
-type integerOperation = { newValue: number };
-type booleanOperation = { newValue: boolean };
-type objectOperation = { newValue: object }; 
-type arrayOperation = { newValue: object };
-type noop = {};
-
-// atomicOperation is a single operation that can be applied in our system
-type atomicOperation = 
-| { type: "stringOperation", stringOperation: stringOperation }
-| { type: "integerOperation", integerOperation: integerOperation }
-| { type: "booleanOperation", booleanOperation: booleanOperation }
-| { type: "objectOperation", objectOperation: objectOperation }
-| { type: "arrayOperation", arrayOperation: arrayOperation }
-| { type: "noop", noop: noop}
-
 // operation is the atomic operation that is sent between clients and servers
-export type Operation = {
-    Path: number[],
-    OperationType: "insert" | "delete",
+export interface Operation {
+  path: number[];
+  opType: "insert" | "delete";
 
-    IsNoOp: boolean
-    Operation: atomicOperation
+  isNoOp: boolean;
+  atomicOp: AtomicOperation;
 }
 
-export const noop: Operation = { 
-    Path: [], 
-    OperationType: "insert", 
-    IsNoOp: true, 
-    Operation:  {
-        type: "noop",
-        noop: {}
+// Represents atomic operations that can be applied to a piece of data of a specific type
+// TODO: in the future update object operation to strictly contain CMS operation data
+export interface AtomicOperation {
+  type: string;
+  transformAgainst: (op: AtomicOperation) => AtomicOperation[];
+}
+
+export class IntegerOperation implements AtomicOperation {
+  type = "integerOperation";
+  newValue: number;
+  constructor(newValue: number) {
+    this.newValue = newValue;
+  }
+
+  transformAgainst = (op: AtomicOperation): AtomicOperation[] => [this, op];
+}
+
+export class BooleanOperation implements AtomicOperation {
+  type = "booleanOperation";
+  newValue: boolean;
+  constructor(newValue: boolean) {
+    this.newValue = newValue;
+  }
+  transformAgainst = (op: AtomicOperation): AtomicOperation[] => [this, op];
+}
+
+export class ObjectOperation implements AtomicOperation {
+  type = "objectOperation";
+  newValue: object;
+  constructor(newValue: object) {
+    this.newValue = newValue;
+  }
+  transformAgainst = (op: AtomicOperation): AtomicOperation[] => [this, op];
+}
+
+export class ArrayOperation implements AtomicOperation {
+  type = "arrayOperation";
+  newValue: object;
+  constructor(newValue: object) {
+    this.newValue = newValue;
+  }
+  transformAgainst = (op: AtomicOperation): AtomicOperation[] => [this, op];
+}
+
+export class StringOperation implements AtomicOperation {
+  type = "stringOperation";
+  start: number;
+  end: number;
+  newValue: string;
+
+  constructor(start: number, end: number, newValue: string) {
+    this.start = start;
+    this.end = end;
+    this.newValue = newValue;
+  }
+
+  transformAgainst = (op: AtomicOperation): AtomicOperation[] => {
+    if (op.constructor.name !== "StringOperation") {
+      return [this, op];
     }
+    const b: StringOperation = op as StringOperation;
+    const [a1, a2, b1, b2] = [copy(this), copy(this), copy(b), copy(b)];
+
+    // This implementation is a TypeScript copy of the server string transform
+    // functions which contain more comments explaining the implementation
+    if (this.newValue != "" && b.newValue != "") {
+      return [insertInsert(b1, a1), insertInsert(a2, b2)];
+    } else if (this.newValue != "" && b.newValue == "") {
+      return [insertDelete(a1, b1), deleteInsert(b2, a2)];
+    } else if (this.newValue == "" && b.newValue != "") {
+      return [deleteInsert(a1, b1), insertDelete(b2, a2)];
+    } else {
+      return [deleteDelete(b1, a1, false), deleteDelete(a2, b2, true)];
+    }
+  };
+}
+
+/**
+ * Return first string operation transformed against second operation when both
+ * are insert operations
+ */
+const insertInsert = (
+  o1: StringOperation,
+  o2: StringOperation
+): StringOperation => {
+  if (o1.start > o2.start) {
+    o1.start += o2.newValue.length;
+    o1.end += o2.newValue.length;
+  }
+  return o1;
 };
 
-// Actual OT transformation functions
-export const transform = (
-    a: Operation,
-    b: Operation
-  ): [Operation, Operation] => {
-    const transformedPaths = transformPaths(a, b);
-    [a.Path, b.Path] = transformedPaths;
-  
-    return [normalise(a), normalise(b)];
-  };
-  
-  /**
-   * Takes in two operations and transforms them accordingly, note that it only
-   * returns the updated paths
-   */
-  const transformPaths = (a: Operation, b: Operation): [number[], number[]] => {
-    const tp = transformationPoint(a.Path, b.Path);
-    if (!effectIndependent(a.Path, b.Path, tp)) {
-      switch (true) {
-        case a.OperationType === "insert" && b.OperationType === "insert":
-          return transformInserts(a.Path, b.Path, tp);
-        case a.OperationType === "delete" && b.OperationType === "delete":
-          return transformDeletes(a.Path, b.Path, tp);
-        case a.OperationType === "insert" && b.OperationType === "delete":
-          return transformInsertDelete(a.Path, b.Path, tp);
-        default:
-          const result = transformInsertDelete(b.Path, a.Path, tp);
-          result.reverse();
-          return result;
+/**
+ * Return first string operation transformed against second operation when first
+ * is insert and second is delete
+ */
+const insertDelete = (
+  o1: StringOperation,
+  o2: StringOperation
+): StringOperation => {
+  if (o1.start <= o2.start) {
+    return o1;
+  } else if (o1.start > o2.end) {
+    o1.start -= o2.newValue.length;
+    o1.end -= o2.newValue.length;
+  } else if (o2.start <= o1.start) {
+    const shift = o1.start - o2.start;
+    o1.start -= shift;
+    o1.end -= shift;
+  }
+  return o1;
+};
+
+/**
+ * Return first string operation transformed against second operation when first
+ * is delete and second is insert
+ */
+const deleteInsert = (
+  o1: StringOperation,
+  o2: StringOperation
+): StringOperation => {
+  return o1;
+};
+
+/**
+ * Return first string operation transformed against second operation when both
+ * are delete operations
+ */
+const deleteDelete = (
+  o1: StringOperation,
+  o2: StringOperation,
+  isLast: boolean
+): StringOperation => {
+  if (o1.start == o2.start && o1.end == o2.end) {
+    if (isLast) {
+      o1.end = o1.start;
+    }
+    return o1;
+  }
+  if (o2.start >= o1.end) {
+    return o1;
+  } else if (o1.start >= o2.end) {
+    const length = o2.end - o2.start;
+    o1.start -= length;
+    o1.end -= length;
+  } else {
+    if (o2.start <= o1.start) {
+      if (o1.end <= o2.end) {
+        o1.end = o1.start;
+      } else {
+        o1.start = o2.end;
       }
+    } else if (o2.end > o1.end) {
+      o1.end = o2.start;
     }
-  
-    return [a.Path, b.Path];
-  };
-  
-  /**
-   * Takes 2 paths and their transformation point and transforms them as if they
-   * were insertion functions
-   */
-  const transformInserts = (
-    a: number[],
-    b: number[],
-    tp: number
-  ): [number[], number[]] => {
-    switch (true) {
-      case a[tp] > b[tp]:
-        return [update(a, tp, 1), b];
-      case a[tp] < b[tp]:
-        return [a, update(b, tp, 1)];
-      default:
-        return a.length > b.length
-          ? [update(a, tp, 1), b]
-          : (a.length < b.length
-            ? [a, update(b, tp, 1)]
-            : [a, b]);
-    }
-  };
-  
-  /**
-   * Takes 2 paths and transforms them as if they were deletion operations
-   */
-  const transformDeletes = (
-    a: number[],
-    b: number[],
-    tp: number
-  ): [number[], number[]] => {
-    switch (true) {
-      case a[tp] > b[tp]:
-        return [update(a, tp, -1), b];
-      case a[tp] < b[tp]:
-        return [a, update(b, tp, -1)];
-      default:
-        return a.length > b.length
-          ? [[], b]
-          : (a.length < b.length
-            ? [a, []]
-            : [[], []]);
-    }
-  };
-  
-  /**
-   * Takes an insertion operation and a deletion operation and transforms them
-   */
-  const transformInsertDelete = (
-    insertOp: number[],
-    deleteOp: number[],
-    tp: number
-  ): [number[], number[]] => {
-    switch (true) {
-      case insertOp[tp] > deleteOp[tp]:
-        return [update(insertOp, tp, -1), deleteOp];
-      case insertOp[tp] < deleteOp[tp]:
-        return [insertOp, update(deleteOp, tp, 1)];
-      default:
-        return insertOp.length > deleteOp.length
-          ? [[], deleteOp]
-          : [insertOp, update(deleteOp, tp, 1)];
-    }
-  };
-  
-  /**
-   * Updates a specific index in a path
-   */
-  const update = (pos: number[], toChange: number, change: number) => {
-    pos[toChange] += change;
-    return pos;
-  };
-  
-  /**
-   * Takes in two paths and computes their transformation point
-   */
-  const transformationPoint = (a: number[], b: number[]): number =>
-    [...Array(Math.min(a.length, b.length)).keys()].find(
-      (i) => a[i] != b[i]
-    ) ?? Math.min(a.length, b.length);
-  
-  /**
-   * Takes two paths and determines if their effect is independent or not
-   */
-  const effectIndependent = (a: number[], b: number[], tp: number): boolean =>
-    (a.length > tp + 1 && b.length > tp + 1) ||
-    (a[tp] > b[tp] && a.length < b.length) ||
-    (a[tp] < b[tp] && a.length > b.length);
-  
-  /**
-   * Normalise turns an empty operation into a noop
-   */
-  const normalise = (a: Operation): Operation => (a.Path.length === 0 ? noop : a);
+  }
+  return o1;
+};
+
+export class NoOp implements AtomicOperation {
+  type = "noOp";
+  transformAgainst = (op: AtomicOperation): AtomicOperation[] => [this, op];
+}
+
+export const noOp: Operation = {
+  path: [],
+  opType: "insert",
+  isNoOp: true,
+  atomicOp: new NoOp(),
+};
+
+/**
+ * Deepcopy an object
+ */
+const copy = <T>(a: T): T => JSON.parse(JSON.stringify(a));
