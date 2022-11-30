2cf1f6421203c1ff081c52d93e687e673d743af3
James Ji <jamesx.ji@gmail.com>
2022-11-30 23:34:34 +1100

    rough layout

--- next/src/components/start/advice/AdviceView.tsx
+++ next/src/components/start/advice/AdviceView.tsx
@@ -1,15 +1,53 @@
+import Image from "next/image";
+import Example from "../../../assets/example.png";
+
+function InfoCardButton({ text }: { text: string }) {
+  return (
+    <button style={{ borderRadius: "5px", height: "40px", width: "100%", backgroundColor: "yellow", display: "flex", justifyContent: "center", alignItems: "center" }}>
+      {text}
+    </button>
+  )
+}
+
+function MainButton({ text }: { text: string }) {
+  return (
+    <button style={{ borderRadius: "5px", height: "60px", width: "320px", backgroundColor: "yellow", display: "flex", justifyContent: "center", alignItems: "center" }}>
+      {text}
+    </button>
+  )
+}
+
 function InfoCard() {
   return (
-    <div style={{}}>
-
+    <div style={{ borderRadius: "5px", backgroundColor: "green", margin: "0 2%", width: "300px", height: "400px" }}>
+      <div style={{ position: "relative", height: "200px", width: "300px" }}>
+        <Image style={{ borderRadius: "5px 5px 0px 0px" }} src={Example} alt="asdfasdf" objectFit="cover" layout="fill" />
+      </div>
+      <div style={{ padding: "20px" }}>
+        <div style={{ fontSize: "32px" }}>
+          Hello
+        </div>
+        <div style={{ padding: "20px 0px", fontSize: "16px" }}>
+          Some random text hello this is a placeholder something
+        </div>
+        <InfoCardButton text={"Link To Guide"} />
+      </div>
     </div>
   )
 }
 
 export default function AdviceView() {
   return (
-    <div style={{ background: "blue", display: "flex", height: "100%" }}>
-      <InfoCard />
+    <div style={{ flex: 1 }}>
+      <div style={{ height: "50%", background: "blue", display: "flex", justifyContent: "space-around" }}>
+        <InfoCard />
+        <InfoCard />
+        <InfoCard />
+        <InfoCard />
+      </div>
+      <div style={{ height: "30%", backgroundColor: "orange", display: "flex", alignItems: "center", justifyContent: "space-around" }}>
+        <MainButton text={"Check out our First Year Guide"} />
+      </div>
     </div>
   );
 }
