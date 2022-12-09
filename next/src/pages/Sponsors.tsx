7ff955f40a50491b7fca2d8f2e6fb42b3fab3239
James <jamest.1402@gmail.com>
2022-12-09 21:06:35 +1100

    fix sponsor width

--- next/src/pages/Sponsors.tsx
+++ next/src/pages/Sponsors.tsx
@@ -78,6 +78,7 @@ export default function Sponsors() {
           justifyContent: 'center',
           alignItems: 'center',
         }}
+        maxWidth={'xl'}
         PaperProps={{
           style: {
             backgroundColor: 'transparent',
