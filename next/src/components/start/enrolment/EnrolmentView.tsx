0260f1e045e73ce7c4f73b16cb3321c9a76bf307
Leonard Chiang <z5311740@student.unsw.edu.au>
2022-12-24 23:31:19 +1100

    completed enrolment features

--- next/src/components/start/enrolment/EnrolmentView.tsx
+++ next/src/components/start/enrolment/EnrolmentView.tsx
@@ -10,25 +10,41 @@ const Circle = styled.div`
   display: flex;
   justify-content: center;
   border-radius: 50%;
-  width: 110px;
-  height: 110px;
+  width: 7vw;
+  height: 7vw;
   background: #BEB8E7;
   border: 8px solid #B6AFE8;
+  margin: 1vw;
+  max-width: 130px;
+  max-height: 130px;
+
+  @media (max-width: 768px) {
+    width: 20vw;
+    height: 20vw;
+    margin: 4vw;
+  }
 `
 
 const ImageContainer = styled.div`
 	position: absolute;
-  width: 50vw;
-  height: 20vh;
-  z-index: 2;
-  top: 32vh;
+  margin: 5vw;
+  width: 80vw;
 
-  @media ${device.laptop} {
-    width: 400px;
-    height: 450px;
-    object-fit: contain;
-    overflow: hidden;
-    border-radius: 8px;
+  top: 35vh;
+  width: 40vw;
+  max-width: 500px;
+  object-fit: contain;
+  overflow: hidden;
+  border-radius: 8px;
+  margin: 0;
+  z-index: 2;  
+
+
+  @media (max-width: 768px) {
+    top: 18.5vh;
+    margin: 5vw;
+    width: 80vw;
+  
   }
 
 `;
@@ -36,50 +52,61 @@ const ImageContainer = styled.div`
 const MainContainer = styled.div`
   display: flex;
   justify-content: center;
-  // width: 70vw;
-  align-items: center;
   height: 70vh;
+
+  padding-top: 10vh;
+
+  @media (max-width: 768px) {
+    min-height: 100vh;
+    text-align: center;
+    flex-direction: column;
+    padding-top: 0;
+  }
 `
 
 const LeftContainer = styled.div`
   display: flex;
   justify-content: center;
-  // align-items: center;
-  // width: 600px;
-  // height: 600px;
-  width: 50%;
+  width: 50vw;
+  @media (max-width: 768px) {
+    flex-direction: column;
+    width: 80vw;
+    min-height: 50vh;
+    align-items: center;
+  }
 `
 
 const RightContainer = styled.div`
   display: flex;
   flex-direction: column;
-  width: 50%;
-  justify-content: space-between;
+  justify-content: space-around;
+  width: 40%;
+  align-items: center;
+  @media (max-width: 768px) {
+    flex: 1;
+    width: 80vw;
+  }
 `
 
-const IconDescription = styled.div`
-  display: flex;
+const IconDescription = styled.p`
   background: #FCF7DE;
   border-radius: 4px;
-  height: 100px;
-  width: 300px;
-  padding: 15px;
+  width: 22vw;
+  padding: 2vw;
+
+  @media (max-width: 768px) {
+    width: 50vw;
+  }
   
 `
 
 const IconHeading = styled.div`
-  display: flex;
   background: #BEB8E7;
-  // justify-content: center;
-  // align-items: center;
   color: #FFF;
   padding: 10px;
-  // height: auto;
-  // width: auto;
-  // height: 40px;
-  // width: 10em;
   border-radius: 4px;
-  font-weight: 600;
+  font-weight: 650;
+  display: table-cell;
 `
 
 const HeadingBlock = styled.div`
@@ -87,38 +114,49 @@ const HeadingBlock = styled.div`
   z-index: 3;
   background: #BEB8E7;
   color: #FFF;
-  padding: 10px;
+  padding: 15px;
   border-radius: 4px;
   font-weight: 800;
-  font-size: 1em;
-  top: 30vh;
+  font-size: 1.2em;
+  top: 32vh;
+  @media (max-width: 768px) {
+    top: 18vh;
+  }
 `
 
 const ResourceContainer = styled.div`
   display: flex;
+  justify-content: center;
+  align-items: center;
 `
 
 const IconInfoContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  // justify-content: space-around;
 `
 
 const EnrolmentDescriptionContainer = styled.div`
   position: absolute;
-  width: 500px;
-  height: 275px;
+  width: 45vw;
+  height: 25vh;
   text-align: center;
   background: #FCF7DE;
   border-radius: 6px;
   z-index: 1;
+  top: 46vh;
+  @media (max-width: 768px) {
+    top: 30.5vh;
+    width: 90vw;
+  }
 
 `
 
 const EnrolmentDescription= styled.p`
   position: absolute;
   bottom: 0;
-  padding: 30px;
+  padding: 2vw;
+`
+
+const Bold = styled.b`
+  font-weight: bold;
 `
 
 export default function EnrolmentView() {
@@ -127,12 +165,12 @@ export default function EnrolmentView() {
             <LeftContainer>
               <HeadingBlock>Read our Enrolment Guide</HeadingBlock>
               <ImageContainer>
-                <Image src={EnrolmentBanner} alt="Enrolment Banner"/>
+                <Image src={EnrolmentBanner} alt="Enrolment Banner" objectFit="contain" />
               </ImageContainer> 
               <EnrolmentDescriptionContainer>
                 <EnrolmentDescription>
                   Just entering a CSE degree at UNSW, and feeling overwhelmed? <br/> <br/>
-                  CSESoc has created the CSE Enrolment Guide help you make sense of it all! Check it out for a comprehensive rundown on everything you need to know about getting started with your degree - including understanding common terms, how majors work, and advice on subject selection and enrolment.
+                  CSESoc has created the <Bold>CSE Enrolment Guide</Bold> help you make sense of it all! Check it out for a comprehensive rundown on everything you need to know about getting started with your degree - including understanding common terms, how majors work, and advice on subject selection and enrolment.
                 </EnrolmentDescription>
               </EnrolmentDescriptionContainer>
             </LeftContainer>
@@ -140,35 +178,33 @@ export default function EnrolmentView() {
             <RightContainer>
               <ResourceContainer>
                 <Circle>
-                  <Image src={NotanglesIcon} alt="Notangles Icon"/>
+                  <Image src={NotanglesIcon} alt="Notangles Icon" width='70vw'/>
                 </Circle>
                 <IconInfoContainer>
                   <IconHeading>Notangles</IconHeading>
-                  <IconDescription>Notangles is a student-led project which helps you build your perfect timetable, even before class registration opens.</IconDescription>
+                  <IconDescription>Notangles is a student-led project which helps you <Bold>build your perfect timetable</Bold>, even before class registration opens.</IconDescription>
                 </IconInfoContainer>
               </ResourceContainer>
 
               <ResourceContainer>
                 <Circle>
-                  <Image src={CirclesIcon} alt="Circles Icon"/>
+                  <Image src={CirclesIcon} alt="Circles Icon" width='40vw'/>
                 </Circle>
                 <IconInfoContainer>
                     <IconHeading>Circles</IconHeading>
-                    <IconDescription>Circles is a student-led project where you can explore your degree structure and plan your degree with ease.</IconDescription>
+                    <IconDescription>Circles is a student-led project where you can <Bold>explore your degree structure</Bold> and <Bold>plan your degree</Bold> with ease.</IconDescription>
                 </IconInfoContainer>
               </ResourceContainer>
 
               <ResourceContainer>
                 <Circle>
-                  <Image src={UNSWIcon} alt="UNSW Icon"/>
+                  <Image src={UNSWIcon} alt="UNSW Icon" width='40vw'/>
                 </Circle>
                 <IconInfoContainer>
                       <IconHeading>UNSW Handbook</IconHeading>
-                      <IconDescription>The handbook is a comprehensive guide to degree programs, specialisations and courses offered at UNSW.</IconDescription>
+                      <IconDescription>The handbook is a comprehensive <Bold>guide to degree programs, specialisations and courses</Bold> offered at UNSW.</IconDescription>
                 </IconInfoContainer>
               </ResourceContainer>
-
-
 
             </RightContainer>
 
