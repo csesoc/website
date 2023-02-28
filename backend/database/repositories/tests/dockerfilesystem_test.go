ece4c6e4858d6b8065ec9fb5ea33996703a1323c
Mae Vuong <63543383+hdphuong@users.noreply.github.com>
2023-02-28 12:27:52 +1100

    test signature (#333)
    * test signature
    
    * test scaffold for repositories

---
+++ backend/database/repositories/tests/dockerfilesystem_test.go
@@ -0,0 +1,62 @@
+package repositories
+
+import (
+	"cms.csesoc.unsw.edu.au/database/repositories"
+
+	"testing"
+)
+
+
+var (
+	unpublishedRepo = repositories.NewUnpublishedRepo()
+	publishedRepo = repositories.NewPublishedRepo()
+)
+
+
+func TestAddToUnpublishedVolume(t *testing.T) {
+	// ==== Test setup ====
+	// ==== Assertions ====
+	// ==== Test teardown ====
+}
+
+func TestAddToPublishedVolume(t *testing.T) {
+	// ==== Test setup ====
+	// ==== Assertions ====
+	// ==== Test teardown ====
+}
+
+func TestRemoveFromUnpublishedVolume(t *testing.T) {
+	// ==== Test setup ====
+	// ==== Assertions ====
+	// ==== Test teardown ====
+}
+
+func TestRemoveFromPublishedVolume(t *testing.T) {
+	// ==== Test setup ====
+	// ==== Assertions ====
+	// ==== Test teardown ====
+}
+
+func TestGetFromUnpublishedVolume(t *testing.T) {
+	// ==== Test setup ====
+	// ==== Assertions ====
+	// ==== Test teardown ====
+}
+
+func TestGetFromPublishedVolume(t *testing.T) {
+	// ==== Test setup ====
+	// ==== Assertions ====
+	// ==== Test teardown ====
+}
+
+func TestDeleteFromUnpublishedVolume(t *testing.T) {
+	// ==== Test setup ====
+	// ==== Assertions ====
+	// ==== Test teardown ====
+}
+
+func TestDeleteFromPublishedVolume(t *testing.T) {
+	// ==== Test setup ====
+	// ==== Assertions ====
+	// ==== Test teardown ====
+}
