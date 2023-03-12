cbb67d425814f43bd31de9f0b013fef3162af8a9
Laura <laurawen003@gmail.com>
2023-03-12 16:42:56 +1100

    possible fix to failing test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import { GlobalStore } from "src/redux-state/index";
 
