42477f957c5beab2bef16ae90b41620ec51ba8f3
Brian Zhang <brian.zhang2101@gmail.com>
2023-05-30 18:23:10 +1000

    added context argument to root retrieval test

--- backend/database/repositories/frontends.go
+++ backend/database/repositories/frontends.go
@@ -1,6 +1,8 @@
 package repositories
 
-import "github.com/google/uuid"
+import (
+	"github.com/google/uuid"
+)
 
 type frontendsRepository struct {
 	embeddedContext
@@ -9,7 +11,7 @@ type frontendsRepository struct {
 const InvalidFrontend = -1
 
 func NewFrontendRepo(frontEndID uuid.UUID, logicalName string, URL string, embeddedContext embeddedContext) (filesystemRepository, error) {
-	err := embeddedContext.ctx.Query("SELECT new_frontend($1, $2, $3)", []interface{}{frontEndID, logicalName, URL})
+	err := embeddedContext.ctx.Query("SELECT new_frontend($1, $2, $3)", []interface{}{frontEndID, logicalName, URL}, &frontEndID)
 	if err != nil {
 		return filesystemRepository{}, err
 	}
@@ -24,7 +26,7 @@ func NewFrontendRepo(frontEndID uuid.UUID, logicalName string, URL string, embed
 // GetFrontendFromURL is the implementation of the frontend repository for frontendRepository
 func (rep frontendsRepository) GetFrontendFromURL(url string) int {
 	var frontendId int
-	err := rep.ctx.Query("SELECT FrontendId from frontend where FrontendUrl = $1;", []interface{}{url}, &frontendId)
+	err := rep.ctx.Query("SELECT ID from frontend where URL = $1;", []interface{}{url}, &frontendId)
 	if err != nil {
 		return InvalidFrontend
 	}
