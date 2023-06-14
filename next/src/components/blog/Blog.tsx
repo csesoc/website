0f151368ba137155bdd7eb98f316d4db65ce4874
Josh Ramos <josh.ramos@student.unsw.edu.au>
2023-06-14 17:20:00 +1000

    removed php support - issue with PrismJs library

--- next/src/components/blog/Blog.tsx
+++ next/src/components/blog/Blog.tsx
@@ -25,7 +25,6 @@ import 'prismjs/components/prism-typescript';
 import 'prismjs/components/prism-tsx';
 import 'prismjs/components/prism-markdown';
 import 'prismjs/components/prism-python';
-// import 'prismjs/components/prism-phpCodeline
 import 'prismjs/components/prism-sql';
 import 'prismjs/components/prism-java';
 import 'prismjs/components/prism-c';
